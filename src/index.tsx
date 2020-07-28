46e7118cf7dba7717032ad3d4a925f4abb8f1cc1
Ihor Magur <ihor.magur@devabit.com>
2020-07-28 14:00:25 +0300

    Default theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {CssBaseline} from "@material-ui/core";
+import {CssBaseline, ThemeProvider} from "@material-ui/core";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = createMuiTheme({});
 
 ReactDOM.render(
-  <React.StrictMode>
-      <CssBaseline>
-          <App />
-      </CssBaseline>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <CssBaseline>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </CssBaseline>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
