6092e4cb9f271f57a7194e62d89473e3cb624286
Ihor Magur <ihor.magur@devabit.com>
2020-07-28 13:58:37 +0300

    Added material

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {CssBaseline} from "@material-ui/core";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <CssBaseline>
+          <App />
+      </CssBaseline>
   </React.StrictMode>,
   document.getElementById('root')
 );
