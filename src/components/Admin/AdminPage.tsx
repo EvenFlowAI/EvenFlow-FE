022bc33c252e469c24bf8bf6ad81c261b8362103
Ihor Magur <ihor.magur@devabit.com>
2020-07-31 14:43:58 +0300

    Included content container

--- src/components/Admin/AdminPage.tsx
+++ src/components/Admin/AdminPage.tsx
@@ -3,12 +3,15 @@ import {Switch, Redirect, Route} from "react-router-dom";
 import {ServiceCenterProfiles} from "./ServiceCenterProfiles/ServiceCenterProfiles";
 import {Locations} from "./Locations/Locations";
 import {Employees} from "./Employees/Employees";
+import {ContentContainer} from "../Content/ContentContainer/ContentContainer";
 
 export const AdminPage = () => {
-    return <Switch>
-        <Route path="/admin/sc-profiles" component={ServiceCenterProfiles} />
-        <Route path="/admin/employees" component={Employees} />
-        <Route path="/admin/locations" component={Locations} />
-        <Redirect to="/admin/sc-profiles" />
-    </Switch>;
+    return <ContentContainer>
+        <Switch>
+            <Route path="/admin/sc-profiles" component={ServiceCenterProfiles}/>
+            <Route path="/admin/employees" component={Employees}/>
+            <Route path="/admin/locations" component={Locations}/>
+            <Redirect to="/admin/sc-profiles"/>
+        </Switch>
+    </ContentContainer>;
 }
