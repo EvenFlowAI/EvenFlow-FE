71d039860dab981ee015eea1f9646410f1db0a4a
Ihor Magur <ihor.magur@devabit.com>
2020-08-07 16:05:36 +0300

    Added employees actions

---
+++ src/components/Admin/Employees/EmployeesActions.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Button} from "@material-ui/core";
+import {Search} from "@material-ui/icons";
+import {SquareIconButton} from "../../UI/Button";
+
+
+export const EmployeesActions = () => {
+    return <>
+        <SquareIconButton variant="outlined">
+            <Search />
+        </SquareIconButton>
+        <Button variant="outlined">
+            Filters
+        </Button>
+    </>;
+}
--- src/components/Content/ContentActions/ContentActions.tsx
+++ src/components/Content/ContentActions/ContentActions.tsx
@@ -3,6 +3,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import {Switch, Route} from "react-router-dom";
 import {Routes} from "../../../config/routes";
 import {DealershipActions} from "../../Admin/DealershipGroups/DealershipActions";
+import {EmployeesActions} from "../../Admin/Employees/EmployeesActions";
 
 
 const useStyles = makeStyles({
@@ -19,6 +20,7 @@ export const ContentActions = () => {
     return <div className={classes.wrapper}>
         <Switch>
             <Route path={Routes.Admin.DealershipGroups} component={DealershipActions} />
+            <Route path={Routes.Admin.Employees} component={EmployeesActions} />
         </Switch>
     </div>;
 }
