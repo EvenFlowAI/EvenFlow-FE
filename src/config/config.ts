9eef2c07f4684f1b654f32d1f69531ee6a1cfac7
Ihor Magur <ihor.magur@devabit.com>
2020-08-17 15:10:07 +0300

    Upd

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export const APIHost = "http://3.129.173.158:8008"
 export const APIUrl = `${APIHost}/api/v0`;
 
-export const defaultRowsPerPageOptions = [1, 10, 50, 100];
-export const defaultRowsPerPage = 1;
+export const defaultRowsPerPageOptions = [10, 50, 100];
+export const defaultRowsPerPage = 10;
