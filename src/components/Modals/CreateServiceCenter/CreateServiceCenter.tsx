c3e4ccbb73e5bea304338961da8607a4c94c021a
Ihor Magur <ihor.magur@devabit.com>
2020-08-19 09:19:58 +0300

    Added form

--- src/components/Modals/CreateServiceCenter/CreateServiceCenter.tsx
+++ src/components/Modals/CreateServiceCenter/CreateServiceCenter.tsx
@@ -1,18 +1,47 @@
 import React from "react";
-import {AvatarContainer, BaseModal, DialogContent, DialogTitle} from "../BaseModal";
+import {AvatarContainer, BaseModal, DialogActions, DialogContent, DialogTitle} from "../BaseModal";
 import {DialogProps} from "../types";
 import {States} from "../../../config/constants";
 import {Autocomplete} from "@material-ui/lab";
 import {TextField} from "../../UI/TextField";
+import {Button, Divider, Grid} from "@material-ui/core";
 
 
-const states = Object.keys(States)
+const states = Object.values(States);
 const ServiceCenterForm: React.FC = () => {
     return <form>
-        <Autocomplete
-            options={states}
-            renderInput={params => <div ref={params.InputProps.ref}><TextField {...params.inputProps} label="Test"/></div>}
-        />
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <TextField label={"Service center name"} fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField label={"Service center email"} fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField label={"Service center phone number"} fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField label={"Contact person email"} fullWidth />
+            </Grid>
+        </Grid>
+        <Divider />
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <TextField label={"Address"} fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField label={"City"} fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+                <Autocomplete
+                    options={states}
+                    renderInput={params => <div ref={params.InputProps.ref}><TextField {...params.inputProps} label="State"/></div>}
+                />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField label="Zip code" />
+            </Grid>
+        </Grid>
     </form>;
 };
 
@@ -23,5 +52,9 @@ export const CreateServiceCenter: React.FC<DialogProps> = props => {
             <AvatarContainer />
             <ServiceCenterForm />
         </DialogContent>
+        <DialogActions>
+            <Button>Cancel</Button>
+            <Button color="primary" variant="contained">Create</Button>
+        </DialogActions>
     </BaseModal>;
 }
