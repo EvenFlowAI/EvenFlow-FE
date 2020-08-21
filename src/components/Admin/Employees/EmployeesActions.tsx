1df786dcb23fed68818beb2ed766a2629360bbb4
Ihor Magur <lik3l@outlook.com>
2020-08-21 08:43:22 +0300

    Changed button name

--- src/components/Admin/Employees/EmployeesActions.tsx
+++ src/components/Admin/Employees/EmployeesActions.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import {Button} from "@material-ui/core";
 import {Search} from "@material-ui/icons";
 import {SquareIconButton} from "../../UI/Button";
+import {useCurrentUser} from "../../../utils/hooks";
 
 
 export const EmployeesActions = () => {
+    const currentUser = useCurrentUser();
     return <>
         <SquareIconButton variant="outlined">
             <Search />
@@ -12,8 +14,8 @@ export const EmployeesActions = () => {
         <Button variant="outlined">
             Filters
         </Button>
-        <Button variant="contained" color="primary" >
-            New
-        </Button>
+        {currentUser && !currentUser.isSuperUser ? <Button variant="contained" color="primary">
+            New Employee
+        </Button> : null}
     </>;
 }
