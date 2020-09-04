c39760dce0ebaa3071246268e1fffddbfa3b3e2e
Ihor Magur <lik3l@outlook.com>
2020-09-04 11:57:09 +0300

    Breaks added

--- src/components/Modals/Breaks/Break.tsx
+++ src/components/Modals/Breaks/Break.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {DialogProps} from "../types";
 import {BaseModal, DialogActions, DialogContent, DialogTitle} from "../BaseModal";
 import {Button, Grid, IconButton} from "@material-ui/core";
-import {TextField} from "../../UI/TextField";
 import {DeleteOutline} from "@material-ui/icons";
 import {makeStyles} from "@material-ui/core/styles";
 import moment from "moment";
+import {ParsableDate} from "@material-ui/pickers/constants/prop-types";
+import {LoadingButton} from "../../UI/Button";
+import {Api} from "../../../config/requests";
+import {useException, useMessage, useSCs} from "../../../utils/hooks";
+import {IBreak, IBreakFrom} from "../../../store/reducers/serviceCenters/types";
+import {timeSpanString} from "../../../config/constants";
+import {TimePicker} from "../../UI/DateTimePickers";
+import {MaterialUiPickersDate} from "@material-ui/pickers/typings/date";
 
 const useStyles = makeStyles({
     button: {
@@ -23,48 +30,176 @@ const useStyles = makeStyles({
     }
 });
 
-const BForm: React.FC<{}> = props => {
+const BForm: React.FC<{
+    form: TBreak[],
+    workDays: number[],
+    onCheck: (day: number, check: boolean) => () => void;
+    onChange: (day: number, t: "from" | "to") => (date: MaterialUiPickersDate) => void
+}> = props => {
     const classes = useStyles();
+
+    const isClosed = (day: number): boolean => {
+        return !props.workDays.includes(day);
+    }
     return <div>
-        {moment.weekdays().map(d =>
-            <Grid container className={classes.container} alignItems="flex-end" key={d}>
+        {moment.weekdays().map((d, dayOfWeek) => {
+            const data = props.form.find(el => el.dayOfWeek === dayOfWeek) || {...blankRow, dayOfWeek};
+            return <Grid container className={classes.container} alignItems="flex-end" key={d}>
                 <Grid item xs={3}>
-                    <Button fullWidth className={classes.button} variant="contained" color="primary">Add Break</Button>
+                    <Button
+                        onClick={props.onCheck(dayOfWeek, true)}
+                        fullWidth
+                        disabled={data.checked || isClosed(dayOfWeek)}
+                        className={classes.button}
+                        variant="contained"
+                        color="primary">
+                        Add Break
+                    </Button>
                 </Grid>
-                <Grid item xs={1} />
+                <Grid item xs={1}/>
                 <Grid item xs={3}>
-                    <TextField
+                    <TimePicker
                         label={d}
                         fullWidth
+                        disabled={!data.checked}
+                        placeholder={isClosed(dayOfWeek) ? "Closed" : ""}
+                        value={data.from}
+                        onChange={props.onChange(dayOfWeek, "from")}
                         id={`${d}Start`}
                         name={`${d}Start`}
                     />
                 </Grid>
-                <Grid item xs={1} className={classes.text}>to</Grid>
+                <Grid item xs={1} className={classes.text}>{
+                    data.checked && !isClosed(dayOfWeek) ? "to" : ""
+                }</Grid>
                 <Grid item xs={3}>
-                    <TextField
-                        label="Monday"
+                    {data.checked && !isClosed(dayOfWeek) ? <TimePicker
                         fullWidth
-                        hideLabel
                         id={`${d}End`}
                         name={`${d}End`}
-                    />
+                        value={data.to}
+                        onChange={props.onChange(dayOfWeek, "to")}
+                    /> : null}
                 </Grid>
                 <Grid item xs={1}>
-                    <IconButton color="primary"><DeleteOutline /></IconButton>
+                    {data.checked ? <IconButton
+                        onClick={props.onCheck(dayOfWeek, false)}
+                        color="primary">
+                        <DeleteOutline/>
+                    </IconButton> : null}
                 </Grid>
-            </Grid>
-        )}
+            </Grid>;
+        })}
     </div>;
 }
-
+type TBreak = {
+    id?: number;
+    from: ParsableDate;
+    to: ParsableDate;
+    checked: boolean;
+    dayOfWeek: number;
+}
+const blankRow: TBreak = {
+    checked: false, from: null, to: null, dayOfWeek: 0
+};
+const initialBreaks: TBreak[] = moment.weekdays().map((day, dayOfWeek) => ({
+    ...blankRow, dayOfWeek
+}));
 export const Break: React.FC<DialogProps> = props => {
+    const [saving, setSaving] = useState<boolean>(false);
+    const [form, setForm] = useState<TBreak[]>(initialBreaks);
+    const [wd, setWD] = useState<number[]>([]);
+    const {selectedSC} = useSCs();
+    const showError = useException();
+    const showMessage = useMessage();
+
+    useEffect(() => {
+        if (props.open && selectedSC) {
+            Api.call<number[]>(
+                Api.endpoints.ServiceCenters.WorkingDays,
+                {urlParams: {id: selectedSC.id}}
+            ).then(({data}) => {
+                setWD(data);
+            });
+            Api.call<IBreak[]>(
+                Api.endpoints.ServiceCenters.GetBreaks,
+                {urlParams: {id: selectedSC.id}}
+            ).then(({data}) => {
+                setForm(initialBreaks.map(el => {
+                    const fd = data.find(e => e.dayOfWeek === el.dayOfWeek);
+                    if (fd) {
+                        return {
+                            ...fd,
+                            checked: true,
+                            from: moment(fd.from, timeSpanString),
+                            to: moment(fd.to, timeSpanString)
+                        }
+                    }
+                    return el;
+                }))
+            })
+        }
+    }, [props.open, setForm, selectedSC, setWD]);
+
+    const handleCheck = (dayOfWeek: number, checked: boolean) => () => {
+        const idx = form.findIndex(d => d.dayOfWeek === dayOfWeek);
+        form[idx] = {...form[idx], checked};
+        setForm([...form]);
+    }
+    const handleChange = (dayOfWeek: number, v: "from" | "to") => (date: MaterialUiPickersDate) => {
+        const idx = form.findIndex(d => d.dayOfWeek === dayOfWeek);
+        form[idx] = {...form[idx], [v]: date};
+        setForm([...form]);
+    }
+
+    const handleSave = async () => {
+        if (!selectedSC) {
+            showError("Service center is not selected");
+        } else {
+            setSaving(true);
+            const data: IBreakFrom = {breaks: form
+                .filter(el => {
+                    return el.checked;
+                }).map(fe => {
+                    return {
+                        ...fe,
+                        from: moment(fe.from).format(timeSpanString),
+                        to: moment(fe.to).format(timeSpanString)
+                    };
+                })};
+            try {
+                await Api.call(
+                    Api.endpoints.ServiceCenters.SetBreaks,
+                    {urlParams: {id: selectedSC.id}, data}
+                )
+                setSaving(false);
+                showMessage("Breaks updated.");
+                props.onClose();
+            } catch (e) {
+                setSaving(false);
+                showError(e);
+            }
+        }
+    }
+
     return <BaseModal {...props} width={780}>
         <DialogTitle onClose={props.onClose}>Edit Breaks</DialogTitle>
-        <DialogContent><BForm /></DialogContent>
+        <DialogContent>
+            <BForm
+                workDays={wd}
+                onChange={handleChange}
+                onCheck={handleCheck}
+                form={form} />
+        </DialogContent>
         <DialogActions>
             <Button onClick={props.onClose}>Cancel</Button>
-            <Button variant="contained" color="primary" onClick={props.onClose}>Save</Button>
+            <LoadingButton
+                loading={saving}
+                variant="contained"
+                color="primary"
+                onClick={handleSave}>
+                Save
+            </LoadingButton>
         </DialogActions>
     </BaseModal>;
 }
