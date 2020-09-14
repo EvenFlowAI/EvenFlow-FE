4b8ea7524031ca722a5e8aff2653723f19ff26ef
Ihor Magur <lik3l@outlook.com>
2020-09-14 08:48:35 +0300

    Added button

--- src/components/Optimizer/OPsCodes/OPsCodesPage.tsx
+++ src/components/Optimizer/OPsCodes/OPsCodesPage.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import {TitleContainer} from "../../Content/TitleContainer/TitleContainer";
 import {optimizerRoot} from "../utils";
+import {Button} from "@material-ui/core";
 
 export const OPsCodesPage = () => {
     return <>
-        <TitleContainer title="Service Requests" pad parent={optimizerRoot} />
+        <TitleContainer
+            title="Service Requests"
+            pad
+            parent={optimizerRoot}
+            actions={
+                <Button
+                    color="primary"
+                    variant="contained"
+                >
+                    Add Ops Code
+                </Button>
+            }
+        />
+
     </>;
 }
