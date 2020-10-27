99afbe24bbb1beebad36fe12eb38e65431d771be
Ihor Magur <lik3l@outlook.com>
2020-10-27 11:13:07 +0200

    Described offer

--- src/store/reducers/offers/types.ts
+++ src/store/reducers/offers/types.ts
@@ -1,3 +1,49 @@
-export interface IOffer {
+import {ParsableDate} from "@material-ui/pickers/constants/prop-types";
 
+export enum EOfferType {
+    AmountOff, PercentOff, FreeService
+}
+export enum ECustomerSegment {
+    All, New, LowValue, MediumValue, HighValue, EndOfWarranty
+}
+export enum EOfferStatus {
+    None, Archived, Deleted
+}
+export enum EOfferState {
+    Active, Disabled
+}
+export enum EDayOfWeek {
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, EveryDay, LastWeekdayInMonth
+}
+export enum ECustomerPresence {
+    DropOff, Waiters, Both
+}
+
+export interface IDuration {
+    start: ParsableDate;
+    end: ParsableDate;
+}
+export interface ITimeOfDay {
+    start: ParsableDate;
+    end: ParsableDate;
+}
+export interface IServiceType {
+    name: string;
+}
+
+export interface IOffer {
+    id: number;
+    title: string;
+    value: number;
+    type: EOfferType;
+    customerSegment: ECustomerSegment;
+    dayOfWeeks: EDayOfWeek[];
+    duration: IDuration;
+    timeOfDay: ITimeOfDay;
+    serviceType: IServiceType;
+    status: EOfferStatus;
+    state: EOfferState;
+    customerPresence: ECustomerPresence;
+    isAllServiceRequestsIncluded: boolean;
+    serviceCenterId: number;
 }
