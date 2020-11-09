92b7d7586df837ce716a4b43807f82a63a37b27a
Ihor Magur <lik3l@outlook.com>
2020-11-09 10:31:05 +0200

    Styled steps

--- src/components/AppointmentFlow/AppointmentFlow.tsx
+++ src/components/AppointmentFlow/AppointmentFlow.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import {VehicleDetailsS1} from "./Steps/VehicleDetailsS1";
-import {Container, Paper, Step, StepButton, StepContent, StepLabel, Stepper} from "@material-ui/core";
+import {Container, Paper, Step, StepButton, StepContent, Stepper} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     container: {
         padding: "40px 0"
     },
@@ -18,8 +18,26 @@ const useStyles = makeStyles({
             fontSize: 16,
             margin: "0 0 22px",
         }
+    },
+    step: {
+        "& span[class*='MuiStepLabel-iconContainer']": {
+            order: 1,
+            padding: "0 0 0 12px",
+            "&>svg": {
+                fontSize: 70,
+                fontWeight: "bold"
+            }
+        },
+        "& span[class*='MuiStepLabel-labelContainer']>span": {
+            fontSize: 16,
+            fontWeight: "bold"
+        },
+        "& span[class*='MuiStepLabel-active']": {
+            color: theme.palette.primary.main
+        }
     }
-});
+}));
+
 type TStep = {
     label: string;
     id: number;
@@ -46,6 +64,7 @@ export const AppointmentFlow = () => {
                 {steps.map(step => {
                     return <Step key={step.id}>
                         <StepButton
+                            className={classes.step}
                             active={activeStep === step.id}
                             onClick={handleStep(step.id)}
                             completed={step.id < activeStep}>
