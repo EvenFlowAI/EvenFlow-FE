1825acf08f42eecb4ebfe6118b9291552fdede25
Ihor Magur <lik3l@outlook.com>
2020-11-11 13:54:53 +0200

    Upd

--- src/components/AppointmentFlow/Steps/ServiceNeedsS2.tsx
+++ src/components/AppointmentFlow/Steps/ServiceNeedsS2.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {FormLabel, IconButton} from "@material-ui/core";
+import React, {useState} from 'react';
+import {FormControlLabel, FormLabel, IconButton, Radio, RadioGroup} from "@material-ui/core";
 import {TextField} from "../UI";
 import {Search} from "@material-ui/icons";
 import {makeStyles} from "@material-ui/core/styles";
@@ -11,17 +11,42 @@ const useStyles = makeStyles({
     },
     btnIcon: {
         marginLeft: 8
+    },
+    item: {
+        justifyContent: "space-between",
+        marginLeft: 0,
+        border: "1px solid gray",
+        borderRadius: 2,
+        paddingLeft: 12,
+        marginTop: 12
     }
 });
 
+type TSRS = {
+    id: number;
+    description: string;
+}
+const srs: TSRS[] = [
+    {id: 1, description: "Engine oil & filter change with inspections"},
+    {id: 2, description: "Engine filter & oil change"},
+    {id: 3, description: "Four wheel alignment and tire rotation"},
+    {id: 4, description: "Engine filter & oil change"},
+    {id: 5, description: "Four wheel alignment and tire rotation"},
+];
 
 export const ServiceNeedsS2 = () => {
+    const [selectedCode, setCode] = useState<number|null>(null);
+    const handleSelectCode = (e: any, value: string) => {
+        setCode(Number(value));
+    }
+
     const classes = useStyles();
     return (
         <div style={{width: "100%"}}>
             <h4>What Does Your Car Need?</h4>
             <FormLabel className={classes.label} htmlFor="search">Search</FormLabel>
             <TextField
+                placeholder="Type here"
                 InputProps={{
                     startAdornment: <IconButton
                         className={classes.btnIcon}
@@ -30,6 +55,21 @@ export const ServiceNeedsS2 = () => {
                     </IconButton>
                 }}
             />
+            <RadioGroup value={selectedCode} onChange={handleSelectCode}>
+                {srs.map(s => {
+                    return <FormControlLabel
+                        className={classes.item}
+                        label={s.description}
+                        labelPlacement={"start"}
+                        value={s.id}
+                        control={
+                            <Radio
+                                color="primary"
+                            />
+                        }
+                    />
+                })}
+            </RadioGroup>
         </div>
     );
 };
