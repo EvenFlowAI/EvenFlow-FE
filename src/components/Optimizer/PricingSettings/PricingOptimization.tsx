5237a5cd5ce8c69a81077bee1a7c4533d9c0be9f
Ihor Magur <ihor.magur@devabit.com>
2021-03-02 15:33:48 +0200

    Added time of year selector

--- src/components/Optimizer/PricingSettings/PricingOptimization.tsx
+++ src/components/Optimizer/PricingSettings/PricingOptimization.tsx
@@ -8,7 +8,7 @@ import {Box, Divider, styled, TableBody, TableCell, TableHead, TableRow} from "@
 import {DenseTable} from "../AppointmentAllocation/UI";
 import moment from "moment";
 import {mappedCalculationsSelector} from "../../../store/reducers/pricingSettings/selectors";
-import {EDay} from "../../../store/reducers/pricingSettings/types";
+import {demandCategories, EDay, EDemandCategory} from "../../../store/reducers/pricingSettings/types";
 import {Autocomplete} from "@material-ui/lab";
 import {TextField} from "../../UI/EndUserInputs";
 import {Label} from "../../AppointmentFlow/UI";
@@ -16,6 +16,7 @@ import {KeyboardArrowDown} from "@material-ui/icons";
 import {useException, useSCs} from "../../../utils/hooks";
 import {loadSCRequestsShort} from "../../../store/reducers/serviceRequests/actions";
 import {RootState} from "../../../store/rootReducer";
+import {TEnumMap} from "../../../store/reducers/utils";
 
 const TableWrapper = styled("div")(({theme}) => ({
     width: "100%",
@@ -29,6 +30,7 @@ const TableWrapper = styled("div")(({theme}) => ({
 
 export const PricingOptimization = () => {
     const [sr, setSr] = useState<IAssignedServiceRequestShort|null>(null);
+    const [demand, setDemand] = useState<TEnumMap<EDemandCategory>|null>({id: EDemandCategory.Average, label: "Average"});
     const dispatch = useDispatch();
     const data = useSelector(mappedCalculationsSelector);
     const srList = useSelector((state: RootState) => state.serviceRequests.scRequestsShort);
@@ -44,39 +46,67 @@ export const PricingOptimization = () => {
     useEffect(() => {
         if (sr) {
             try {
-                dispatch(loadPricingCalculations(sr.id));
+                dispatch(loadPricingCalculations(sr.id, demand?.id));
             } catch (e) {
                 showError(e);
             }
         }
-    }, [sr, dispatch, showError]);
+    }, [sr, demand, dispatch, showError]);
 
     const handleChange = (e: any, value: IAssignedServiceRequestShort|null) => {
         setSr(value);
+    }
+    const handleDemandChange = (e: any, value: TEnumMap<EDemandCategory>|null) => {
+        setDemand(value);
     }
 
     return <SquarePaper variant="outlined">
         <PaperTitle>Check Pricing</PaperTitle>
         <Divider />
-        <Box p={2} mt={1.5} width={300}>
-            <Label style={{color: "#252733"}} htmlFor="serviceRequest">
-                Select Service Request
-            </Label>
-            <Autocomplete
-                id="serviceRequest"
-                placeholder="Select here"
-                popupIcon={<KeyboardArrowDown />}
-                onChange={handleChange}
-                options={srList}
-                getOptionLabel={option => option.code}
-                renderInput={params => <TextField {...{
-                    ...params, InputProps: {
-                        ...params.InputProps,
-                        disableUnderline: true,
-                        style: {padding: "4px 10px"}
-                    }
-                }} placeholder="Select here" />}
-            />
+        <Box display="flex" flexWrap={"wrap"}>
+            <Box p={2} mt={1.5} width={300}>
+                <Label style={{color: "#252733"}} htmlFor="serviceRequest">
+                    Select Service Request
+                </Label>
+                <Autocomplete
+                    id="serviceRequest"
+                    placeholder="Select here"
+                    popupIcon={<KeyboardArrowDown/>}
+                    onChange={handleChange}
+                    options={srList}
+                    value={sr}
+                    getOptionLabel={option => option.code}
+                    renderInput={params => <TextField {...{
+                        ...params, InputProps: {
+                            ...params.InputProps,
+                            disableUnderline: true,
+                            style: {padding: "4px 10px"}
+                        }
+                    }} placeholder="Select here"/>}
+                />
+            </Box>
+            <Box p={2} mt={1.5} width={300}>
+                <Label style={{color: "#252733"}} htmlFor="timeOfYear">
+                    Select time of a year
+                </Label>
+                <Autocomplete
+                    id="timeOfYear"
+                    placeholder="Select here"
+                    popupIcon={<KeyboardArrowDown/>}
+                    value={demand}
+                    onChange={handleDemandChange}
+                    options={demandCategories}
+                    getOptionLabel={option => option.label}
+                    getOptionSelected={(option, value) => option.id === value.id}
+                    renderInput={params => <TextField {...{
+                        ...params, InputProps: {
+                            ...params.InputProps,
+                            disableUnderline: true,
+                            style: {padding: "4px 10px"}
+                        }
+                    }} placeholder="Select here"/>}
+                />
+            </Box>
         </Box>
         <Box mt={2} ml={2}>
             <PaperTitle noPadding>time of a day</PaperTitle>
