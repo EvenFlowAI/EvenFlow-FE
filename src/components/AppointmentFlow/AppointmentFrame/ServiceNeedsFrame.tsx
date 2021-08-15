9bb272453798bdfda65918c1ddddc0135cc97ec3
Ihor Magur <ihor.magur@devabit.com>
2021-08-15 13:46:10 +0300

    Added cards

--- src/components/AppointmentFlow/AppointmentFrame/ServiceNeedsFrame.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/ServiceNeedsFrame.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Actions} from "./Actions";
 import { StepWrapper } from './StepWrapper';
 import {styled} from "@material-ui/core";
+import tireIcon from "../../../assets/img/tire-rotation-icon.png";
+import worksIcon from "../../../assets/img/oil-icon.png";
+import recallIcon from "../../../assets/img/recallIcon.png";
+import moreIcon from "../../../assets/img/tellUsMoreIcon.png";
 
 
 const CardsWrapper = styled("div")({
     display: "flex",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
-    gap: "18px"
+    gap: "18px",
 });
 
 const CardWrapper = styled("div")({
@@ -16,14 +20,28 @@ const CardWrapper = styled("div")({
     gridTemplateColumns: "1fr",
     gridTemplateRows: "1fr 1fr",
     width: "100%",
-    height: "100%",
-    border: "1px solid #DADADA"
+    border: "1px solid #DADADA",
+    fontSize: 24,
+    textAlign: "center",
+    alignItems: "center",
+    padding: 10,
 });
 
-const ServiceCard = () => {
+type TCard = {
+    label: string;
+    icon: string;
+}
+const cards: TCard[] = [
+    {label: "Factory or Dealer Scheduled Maintenance", icon: tireIcon},
+    {label: "The Works Quick Lane Checkup", icon: worksIcon},
+    {label: "Recall", icon: recallIcon},
+    {label: "Tell us more", icon: moreIcon},
+]
+
+const ServiceCard: React.FC<{card: TCard}> = ({card}) => {
     return <CardWrapper>
-        <span>icon</span>
-        <span>Factory or Dealer Scheduled Maintenance</span>
+        <span><img src={card.icon} alt={card.label}/></span>
+        <span>{card.label}</span>
     </CardWrapper>
 }
 
@@ -31,7 +49,9 @@ export const ServiceNeedsFrame = () => {
     return (
         <StepWrapper>
             <CardsWrapper>
-                <ServiceCard />
+                {cards.map(card => {
+                    return <ServiceCard card={card} key={card.label} />
+                })}
             </CardsWrapper>
             <Actions onNext={() => {}} onBack={() => {}} />
         </StepWrapper>
