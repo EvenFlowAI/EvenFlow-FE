f30984ab053cb04a4e24bb9e4b679ac2188fcec4
Ihor Magur <ihor.magur@devabit.com>
2021-08-15 16:50:05 +0300

    Restored first step

--- src/components/Layout/AppointmentFrameLayout.tsx
+++ src/components/Layout/AppointmentFrameLayout.tsx
@@ -33,8 +33,7 @@ const SidebarWrapper = styled('div')({
 });
 
 export const AppointmentFrameLayout = () => {
-    // TODO: Replace with 'carSelection'
-    const [currentScreen, setCurrentScreen] = useState<TScreen>("appointmentConfirmation");
+    const [currentScreen, setCurrentScreen] = useState<TScreen>("carSelection");
 
     const handleChangeScreen = useCallback((name: TScreen) => () => {
         setCurrentScreen(name);
