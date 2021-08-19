4132b29f663357dd5f5703e1b9db40ad2c801d93
Ihor Magur <ihor.magur@devabit.com>
2021-08-19 12:31:08 +0300

    Added subService

--- src/store/reducers/appointmentFrameReducer/actions.ts
+++ src/store/reducers/appointmentFrameReducer/actions.ts
@@ -2,3 +2,4 @@ import {createAction} from "@reduxjs/toolkit";
 import {TServiceCard} from "./types";
 
 export const selectService = createAction<TServiceCard>("fAppointment/selectService");
+export const selectSubService = createAction<TServiceCard>("fAppointment/selectSubService");
--- src/store/reducers/appointmentFrameReducer/reducer.ts
+++ src/store/reducers/appointmentFrameReducer/reducer.ts
@@ -1,16 +1,21 @@
 import {TServiceCard} from "./types";
 import {createReducer} from "@reduxjs/toolkit";
-import {selectService} from "./actions";
+import {selectService, selectSubService} from "./actions";
 
 type TState = {
-    service: TServiceCard|null
+    service: TServiceCard|null;
+    subService: TServiceCard|null;
 }
 const initialState: TState = {
-    service: null
+    service: null,
+    subService: null,
 };
 
 export const appointmentFrameReducer = createReducer(initialState, builder => builder
     .addCase(selectService, (state, {payload}) => {
+        return {...state, service: payload, subService: null};
+    })
+    .addCase(selectSubService, (state, {payload}) => {
         return {...state, service: payload};
     })
 )
