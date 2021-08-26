003d808a94b6e17a0d2e05d63db0843ff41d9746
Ihor Magur <ihor.magur@devabit.com>
2021-08-26 12:05:22 +0300

    Added consultant

--- src/components/AppointmentFlow/AppointmentFrame/confirmationSections/Review.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/confirmationSections/Review.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {ConfirmationTitle} from "../Title";
 import {styled} from "@material-ui/core";
+import {useSelector} from "react-redux";
+import {RootState} from "../../../../store/rootReducer";
 
 const Wrapper = styled('ul')({
     display: "flex",
@@ -23,14 +25,20 @@ const ButtonLink = styled('div')({
 })
 
 export const Review = () => {
+    const [vehicle, sc, ssc, consultant] = useSelector((state: RootState) => [
+        state.appointmentFrame.selectedVehicle,
+        state.appointmentFrame.service,
+        state.appointmentFrame.subService,
+        state.appointmentFrame.advisor
+    ]);
     return (
         <div>
             <ConfirmationTitle>Review</ConfirmationTitle>
             <Wrapper>
-                <li>2019 Ford Focus</li>
-                <li>The Works Preferred</li>
-                <li>Transportation needs: Take the shuttle  </li>
-                <li>Consultant: Mike Graham</li>
+                <li>{vehicle?.year} {vehicle?.make} {vehicle?.model}</li>
+                <li>{ssc?.name ?? sc?.name}</li>
+                <li>Transportation needs: Take the shuttle</li>
+                <li>Consultant: {consultant?.name ?? "Any Available"}</li>
             </Wrapper>
             <ButtonLink>View Details</ButtonLink>
         </div>
