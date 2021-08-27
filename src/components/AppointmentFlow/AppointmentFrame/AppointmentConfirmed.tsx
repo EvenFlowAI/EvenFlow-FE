416b4a9513d4718d9bb8f3c2d1fec39c4f9e71e2
Ihor Magur <ihor.magur@devabit.com>
2021-08-27 12:34:03 +0300

    Upd

--- src/components/AppointmentFlow/AppointmentFrame/AppointmentConfirmed.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/AppointmentConfirmed.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import {StepWrapper} from "./StepWrapper";
+import {styled} from "@material-ui/core";
+
+
+const Wrapper = styled('div')({
+    boxShadow: "1px 5px 15px rgba(0, 0, 0, 0.25);",
+    padding: 20,
+    "& h2": {
+        textTransform: "uppercase",
+        margin: 0,
+        padding: 0
+    }
+})
 
 export const AppointmentConfirmed = () => {
     return <StepWrapper>
-        Confirmed
+        <Wrapper>
+            <h2>Appointment Confirmed!</h2>
+        </Wrapper>
     </StepWrapper>
 };
