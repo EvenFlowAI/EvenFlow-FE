4952c8846511170602517e1f774f562b5e0952ee
Ihor Magur <ihor.magur@devabit.com>
2021-08-28 18:09:08 +0300

    Changed options

--- src/components/AppointmentFlow/AppointmentFrame/MaintenanceDetails.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/MaintenanceDetails.tsx
@@ -5,7 +5,6 @@ import {styled} from "@material-ui/core";
 import {StepWrapper} from "./StepWrapper";
 import {Actions} from "./Actions";
 import {TActionProps} from "./types";
-import {TOption} from "../../../types/types";
 import {useDispatch, useSelector} from "react-redux";
 import {TMaintenanceDetails} from "../../../store/reducers/appointmentFrameReducer/types";
 import {setMaintenanceDetails} from "../../../store/reducers/appointmentFrameReducer/actions";
@@ -21,24 +20,24 @@ const SelectWrapper = styled('div')({
 type TSelect = {
     label: string;
     name: keyof TMaintenanceDetails;
-    options: TOption[];
+    options: string[];
 };
 
 
-const mileageOptions: TOption[] = [
-    {value: "3000", name: "3000"},
-    {value: "5000", name: "5000"},
-    {value: "10000", name: "10000"},
-    {value: "15000", name: "15000"},
-    {value: "25000", name: "25000"},
-    {value: "30000", name: "30000"},
-    {value: "40000", name: "40000"},
-    {value: "50000", name: "50000"},
-    {value: "60000", name: "60000"},
-    {value: "70000", name: "70000"},
-    {value: "80000", name: "80000"},
-    {value: "90000", name: "90000"},
-    {value: "100000", name: "100000"},
+const mileageOptions: string[] = [
+    "3000",
+    "5000",
+    "10000",
+    "15000",
+    "25000",
+    "30000",
+    "40000",
+    "50000",
+    "60000",
+    "70000",
+    "80000",
+    "90000",
+    "100000",
 ];
 const selects: TSelect[] = [
     {label: "Year", name: "year", options: []},
@@ -52,8 +51,8 @@ const selects: TSelect[] = [
 export const MaintenanceDetails: React.FC<TActionProps> = ({onNext, onBack}) => {
     const dispatch = useDispatch();
     const maintenanceDetails = useSelector((state: RootState) => state.appointmentFrame.maintenanceDetails);
-    const handleChange = (name: keyof TMaintenanceDetails) => (e: React.ChangeEvent<{}>, option: TOption|null) => {
-        dispatch(setMaintenanceDetails({[name]: option?.value ?? null}))
+    const handleChange = (name: keyof TMaintenanceDetails) => (e: React.ChangeEvent<{}>, option: string|null) => {
+        dispatch(setMaintenanceDetails({[name]: option ?? null}))
     }
     return (<StepWrapper>
         <SelectWrapper>
@@ -62,18 +61,12 @@ export const MaintenanceDetails: React.FC<TActionProps> = ({onNext, onBack}) => 
                     key={select.name}
                     options={select.options}
                     onChange={handleChange(select.name)}
-                    getOptionLabel={option => option.name}
-                    getOptionSelected={(option, value) => option.value === value.value}
                     fullWidth
                     autoComplete={true}
                     renderInput={autocompleteRender({
                         label: select.label, placeholder: `Select ${select.label}`
                     })}
-                    value={
-                        maintenanceDetails[select.name]
-                            ? select.options.find(o => o.value === maintenanceDetails[select.name]) ?? null
-                            : null
-                    }
+                    value={maintenanceDetails[select.name] ?? null}
                 />
             })}
         </SelectWrapper>
