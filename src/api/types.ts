6c5ba31fa00119991236f706448fe0cdb3b0769e
Ihor Magur <ihor.magur@devabit.com>
2021-08-30 13:14:58 +0300

    Added typing for package

--- src/api/types.ts
+++ src/api/types.ts
@@ -11,23 +11,33 @@ import {IServiceRequest, IServiceRequestShort} from "../store/reducers/serviceRe
 import {ICurrentUser} from "../store/reducers/users/types";
 import {TEnumKeyLabel} from "../store/reducers/utils";
 
-export type TApiResponse<R=any> = Promise<AxiosResponse<R>>;
-export type TApiEndpoint<T=any, R=any> = (arg: T) => TApiResponse<R>;
+export type TApiResponse<R = any> = Promise<AxiosResponse<R>>;
+export type TApiEndpoint<T = any, R = any> = (arg: T) => TApiResponse<R>;
 export type TApiView = Record<string, TApiEndpoint>;
 
 export type TApi = Record<string, TApiView>;
+
 export enum EServiceCategoryPage {
-    Page1="Page1",
-    Page2="Page2"
+    Page1 = "Page1",
+    Page2 = "Page2"
 }
+
 export enum EVehiclePropType {
     Make, Model, Transmission, DriveType, EngineType
 }
 
+export enum ECustomerCriteria {
+    Any, Own, Lease
+}
+
+export enum EMaintenanceOptionType {
+    Base, Value, Preferred
+}
+
 export interface ICreateAppointment {
     id?: number;
-    serviceCategoryId: number|null,
-    maintenancePackageOptionId: number|null;
+    serviceCategoryId: number | null,
+    maintenancePackageOptionId: number | null;
     date: ParsableDate;
     slot: string;
     customerId?: string;
@@ -36,18 +46,18 @@ export interface ICreateAppointment {
     appointmentTimingType: EAppointmentTimingType;
     driver: IPersonalInformation;
     serviceCenterId: number;
-    offerId: number|null;
+    offerId: number | null;
     transportationNeeds: {
         isNeed: boolean;
         description: string;
     },
     vehicle: {
-        dmsId: string|null;
+        dmsId: string | null;
         vin: string;
         make: string;
-        year: string|null;
+        year: string | null;
         model: string,
-        mileage: string|null;
+        mileage: string | null;
         transmission: string;
         driveType: string;
         engineType: string;
@@ -56,9 +66,11 @@ export interface ICreateAppointment {
     comment: string;
     serviceRequestIds: number[];
 }
+
 export interface IUpdateAppointment extends ICreateAppointment, ICreateAppointmentResp {
     id: number;
 }
+
 export interface ICreateAppointmentResp {
     id: number;
     hashKey: string;
@@ -73,6 +85,7 @@ export interface ICustomerLoadedData {
     phoneNumbers: string[];
     vehicles: ILoadedVehicle[];
 }
+
 export interface ILoadedVehicle {
     dmsId: string;
     vin: string;
@@ -83,17 +96,25 @@ export interface ILoadedVehicle {
     warrantyExpiration?: ParsableDate;
     appointmentHashKeys?: string[];
 }
-export interface IPasswordRecoveryData { email: string; }
-export interface IPasswordRecoveryResp { }
+
+export interface IPasswordRecoveryData {
+    email: string;
+}
+
+export interface IPasswordRecoveryResp {
+}
+
 export interface ISetNewPasswordData {
     userId: string;
     token: string;
     newPassword: string;
 }
+
 export interface IConfig {
     roles: string[];
     timeZones: string[];
 }
+
 export interface IListAppointmentRequest {
     serviceCenterId: number;
     offerId?: number;
@@ -103,11 +124,13 @@ export interface IListAppointmentRequest {
     orderBy?: "requestDate" | "date" | "transactionValue";
     isAscending?: boolean;
 }
+
 export interface IDriverInfo {
     fullName: string;
     phoneNumber: string;
     email: string;
 }
+
 export interface ITransportationNeeds {
     isNeed: boolean;
     description: string;
@@ -116,6 +139,7 @@ export interface ITransportationNeeds {
 export enum AppointmentStatus {
     Active, Cancelled
 }
+
 export const appointmentStatuses: TEnumKeyLabel<AppointmentStatus> = {
     [AppointmentStatus.Active]: "Active",
     [AppointmentStatus.Cancelled]: "Canceled"
@@ -147,19 +171,24 @@ export interface IListAppointment {
     createdBy: string;
     user?: ICurrentUser;
 }
+
 export interface ISearchCustomerParams {
     serviceCenterId: number;
     searchTerm: string;
 }
+
 export interface ISearchTerm {
     searchTerm: string;
 }
+
 export interface ISecurityCode {
     securityCode: string;
 }
+
 export interface IServiceCenterId {
     serviceCenterId: number;
 }
+
 export interface ISessionId {
     "session-id": string;
 }
@@ -172,10 +201,12 @@ export interface IServiceCategory {
     loadedIcon?: JSX.Element | string;
     serviceRequests: IServiceRequest[];
 }
+
 export interface IServiceConsultantShort {
     id: string;
     name: string
 }
+
 export interface IServiceConsultant {
     id: string;
     name: string;
@@ -184,13 +215,65 @@ export interface IServiceConsultant {
     position: string;
     iconPath: string;
 }
+
 export interface ICustomer {
     fullName: string;
     phoneNumber: string;
     email: string;
 }
+
 export interface ITransportation {
     type: number;
     name: string;
     description: string;
+}
+
+export interface IYearRange {
+    from: number;
+    to: number;
+}
+
+export interface IBusinessRule {
+    vehicleMakes: string[];
+    vehicleModels: string[];
+    vehicleYearRange: IYearRange;
+    vehicleMileageRange: IYearRange;
+    customerCriteria: ECustomerCriteria;
+}
+
+export interface IServiceRequestAssigned {
+    type: EMaintenanceOptionType;
+    serviceRequestId: number;
+}
+
+export interface IComplimentaryService {
+    id: number;
+    name: string;
+    price: number;
+    durationInHours: number;
+    code: string;
+}
+
+export interface IPackageOptions {
+    id: number;
+    type: EMaintenanceOptionType;
+    serviceRequestPrice: number;
+    complimentaryServicePrice: number;
+    serviceRequestLaborHours: number;
+    complimentaryServiceLaborHours: number;
+    price: number;
+    maintenancePackageId: number;
+    serviceRequests: number[];
+    complimentaryServices: number[];
+}
+
+export interface IPackage {
+    id: number;
+    name: string;
+    isApplyPricingOptimization: boolean;
+    businessRules: IBusinessRule;
+    serviceRequests: IServiceRequest[];
+    serviceRequestsAssigned: IServiceRequestAssigned[];
+    complimentaryServices: IComplimentaryService[];
+    options: IPackageOptions[];
 }
