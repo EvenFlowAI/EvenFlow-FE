e517ba92bbddd108c3161909fe8508088570d516
Alisa44 <alisa444.86@gmail.com>
2021-10-11 19:01:12 +0300

    fields for add package modal added

--- src/components/Modals/AddPackage/AddPackage.tsx
+++ src/components/Modals/AddPackage/AddPackage.tsx
@@ -5,12 +5,15 @@ import {makeStyles} from "@material-ui/core/styles";
 import {TextField} from "../../UI/TextField";
 import {PlusOneRounded} from "@material-ui/icons";
 import {IconButton, Button} from "@material-ui/core";
+import OpsCode from "./parts/OpsCodeLabel";
+import {Autocomplete} from "@material-ui/lab";
+import {autocompleteRender} from "../../UI/AutocompleteRender";
 
 type TModalProps = DialogProps;
 
 interface IVehiclesData {
-    make: [] | null;
-    model: [] | null;
+    make: string[] | undefined;
+    model: string[] | undefined;
     mileageFrom: number | null;
     mileageTo: number | null;
     yearFrom: number |null;
@@ -65,8 +68,8 @@ const useStyles = makeStyles(() => ({
 }))
 
 const initialValues = {
-    make: null,
-    model: null,
+    make: undefined,
+    model: undefined,
     mileageFrom: null,
     mileageTo: null,
     yearFrom: null,
@@ -102,16 +105,26 @@ const AddPackage: React.FC<TModalProps> = (props) => {
 
     }, [])
 
+    const openOpsCodesModal = useCallback((): void => {
+
+    }, [])
+
     const addComplimentary = useCallback((): void => {
 
     }, [])
 
-    const onFormFieldChange = useCallback((value: string | number | string[], fieldName: 'string'): void => {
-        setVehiclesData(prevData => ({...prevData, [fieldName]: value}))
+    const onFormFieldChange = useCallback(
+        (fieldName: keyof IVehiclesData) =>
+        (e: React.ChangeEvent<{}>, value: string[] | number | null): void => {
+       setVehiclesData((prevData: IVehiclesData) => ({...prevData, [fieldName]: value}))
     }, [])
 
+    const onDelete = (code: number): void => {
+        setOpsCodes(prev => prev.filter(item => item !== code));
+    }
+
     return (
-        <BaseModal {...props}>
+        <BaseModal {...props} width={460}>
             <DialogTitle onClose={handleClose}>Add Maintenance Package</DialogTitle>
             <DialogContent>
                 <TextField label='Maintenance Package Name' onChange={onNameChange} value={packageName}/>
@@ -126,13 +139,16 @@ const AddPackage: React.FC<TModalProps> = (props) => {
                     Assign Ops Code To Package
                 </Button>
                 <div className={opsCodes?.length ? classes.opsCodesWrapper : classes.emptyOpsCodes}>
-                    {/*{opsCodes?.length ?}*/}
+                    { opsCodes?.length
+                        ? opsCodes.map(item => <OpsCode code={item} onDelete={onDelete}/>)
+                        : <p>There are no ops codes in this list yet</p>
+                    }
                 </div>
                 <span className={classes.label}>Add Ops Code To Package</span>
                 <div className={classes.btnsWrapper}>
                     <Button
                         color="primary"
-                        onClick={addOpsCode}>
+                        onClick={openOpsCodesModal}>
                         Add Ops Codes
                     </Button>
                     <Button
@@ -142,6 +158,50 @@ const AddPackage: React.FC<TModalProps> = (props) => {
                     </Button>
                 </div>
                 <div className={classes.formWrapper}>
+                    <Autocomplete
+                        multiple
+                        options={['BMW', 'Ford', 'Infinity']}
+                        disableCloseOnSelect
+                        value={vehiclesData?.make}
+                        onChange={onFormFieldChange('make')}
+                        renderInput={autocompleteRender({label: "Make", fullWidth: true, placeholder: 'Select Make'})}
+                    />
+                    <Autocomplete
+                        multiple
+                        options={['2Series', '3Series', '5Series']}
+                        disableCloseOnSelect
+                        value={vehiclesData?.model}
+                        onChange={onFormFieldChange('model')}
+                        renderInput={autocompleteRender({label: "Model", fullWidth: true, placeholder: 'Select Model'})}
+                    />
+                    <Autocomplete
+                        options={[1000, 10000, 20000]}
+                        disableCloseOnSelect
+                        value={vehiclesData?.mileageFrom}
+                        onChange={onFormFieldChange('mileageFrom')}
+                        renderInput={autocompleteRender({label: "Mileage", placeholder: 'From'})}
+                    />
+                    <Autocomplete
+                        options={[1000, 10000, 20000]}
+                        disableCloseOnSelect
+                        value={vehiclesData?.mileageTo}
+                        onChange={onFormFieldChange('mileageTo')}
+                        renderInput={autocompleteRender({label: '', placeholder: 'To'})}
+                    />
+                    <Autocomplete
+                        options={[1997, 1998, 2000]}
+                        disableCloseOnSelect
+                        value={vehiclesData?.yearFrom}
+                        onChange={onFormFieldChange('yearFrom')}
+                        renderInput={autocompleteRender({label: '', placeholder: 'From'})}
+                    />
+                    <Autocomplete
+                        options={[1997, 1998, 2000]}
+                        disableCloseOnSelect
+                        value={vehiclesData?.yearTo}
+                        onChange={onFormFieldChange('yearTo')}
+                        renderInput={autocompleteRender({label: '', placeholder: 'To'})}
+                    />
                 </div>
             </DialogContent>
         </BaseModal>
--- src/components/Modals/AddPackage/parts/OpsCodeLabel.tsx
+++ src/components/Modals/AddPackage/parts/OpsCodeLabel.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import {DeleteOutlined} from "@material-ui/icons";
+import {makeStyles} from "@material-ui/core/styles";
 
 type TOpsCode = {
-    onDelete: () => void;
-    code: object | null,
+    onDelete: (code: number) => void;
+    code: number,
 }
 
+const useStyles = makeStyles(() => ({
+    wrapper: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    icon: {
+        marginLeft: 5,
+    }
+}))
+
 const OpsCode: React.FC<TOpsCode> = ({ onDelete, code }) => {
-    return <div></div>
+    const classes = useStyles();
+    return <div className={classes.wrapper}>{code}
+    <DeleteOutlined onClick={() => onDelete(code)} className={classes.icon}/></div>
 }
+
+export default OpsCode;
