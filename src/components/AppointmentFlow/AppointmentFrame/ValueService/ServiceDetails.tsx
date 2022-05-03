31ef1f52aa8e5a76360ada678e0be47581f3e68f
Alisa44 <alisa444.86@gmail.com>
2022-05-03 12:17:06 +0200

    1321 fixed

--- src/components/AppointmentFlow/AppointmentFrame/ValueService/ServiceDetails.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/ValueService/ServiceDetails.tsx
@@ -15,7 +15,7 @@ type TServiceDetails = {
 const Description = styled('div')(() => ({
     padding: 10,
     marginBottom: 20,
-    "& > p": {
+    "& > p:not(:last-child)": {
         fontWeight: 600,
         color: "#828282",
     }
