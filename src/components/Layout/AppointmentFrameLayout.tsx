c5357e5453bebbfff308728eb96efd9681d5bd38
Alisa44 <alisa444.86@gmail.com>
2022-05-16 11:14:30 +0200

    log event1

--- src/components/Layout/AppointmentFrameLayout.tsx
+++ src/components/Layout/AppointmentFrameLayout.tsx
@@ -127,6 +127,8 @@ export const AppointmentFrameLayout = () => {
     useEffect(() => {
         if (!trackerCreated) {
             window.addEventListener('message', function(event) {
+                // todo delete
+                console.log(event)
                 if (!prodParentLinks.includes(event.origin)) return;
                 let originSite = event.origin;
                 if (window.location?.ancestorOrigins?.length) originSite = window.location.ancestorOrigins[0];
