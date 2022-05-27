3c6c072a708684ff8a73a2ccdb67ecd525e8ed7f
Alisa44 <alisa444.86@gmail.com>
2022-05-27 15:46:59 +0200

    new icon added

--- src/components/Admin/Dashboard/Dashboard.tsx
+++ src/components/Admin/Dashboard/Dashboard.tsx
@@ -30,6 +30,7 @@ import {ReactComponent as ServiceCodes} from "../../../assets/img/serviceOpsCode
 import {ReactComponent as Transportation} from "../../../assets/img/transportation_options.svg";
 import {ReactComponent as Vehicle} from "../../../assets/img/vehicleDetails.svg";
 import {ReactComponent as Calendar} from "../../../assets/img/date_1_grey.svg";
+import {ReactComponent as OffFlag} from "../../../assets/img/Off_Flag_Appointments.svg";
 import Reminders from "../../Modals/Reminders/Reminders";
 import ManageAppointments from "../../Modals/ManageAppointments/ManageAppointments";
 
@@ -243,7 +244,7 @@ export const AdminDashboard: React.FC = () => {
         {label: "Service Ops Codes Mapping", icon: <ServiceCodes />, action: onOpenServicesOps},
         {label: "Vehicle Detail Options", icon: <Vehicle />, action: onOpenVehicle},
         {label: "Appointment Reminders", icon: <Calendar />, action: onOpenReminders},
-        {label: "Manage Ex EvenFlow Appointments", icon: <Calendar/>, action: onOpenManageAppointments},
+        {label: "Manage Ex EvenFlow Appointments", icon: <OffFlag/>, action: onOpenManageAppointments},
         // {label: "Customer Verification", icon: <Customer/>, action: onOpenVerification},
     ];
 
