9e90b5c0bd553dda2ea7804ea764ec7f6f0664f8
Alisa44 <alisa444.86@gmail.com>
2022-06-08 13:12:41 +0200

    deleted initial BF settings data

--- src/store/reducers/bookingFlowConfig/reducer.ts
+++ src/store/reducers/bookingFlowConfig/reducer.ts
@@ -1,30 +1,6 @@
-import {EServiceTypeBookingFlow, TServiceTypeSettings} from "./types";
+import {TServiceTypeSettings} from "./types";
 import {createReducer} from "@reduxjs/toolkit";
 import {setBookingFlowConfig, setBookingFlowConfigLoading} from "./actions";
-
-export const initialData: TServiceTypeSettings[] = [
-    {
-        available: true,
-        valueService: true,
-        productPageForValueService: true,
-        advisorSelection: true,
-        serviceType: EServiceTypeBookingFlow.VisitCenter
-    },
-    {
-        available: false,
-        valueService: true,
-        productPageForValueService: true,
-        advisorSelection: false,
-        serviceType: EServiceTypeBookingFlow.MobileService
-    },
-    {
-        available: false,
-        valueService: true,
-        productPageForValueService: true,
-        advisorSelection: true,
-        serviceType: EServiceTypeBookingFlow.PickUpDropOff
-    },
-]
 
 interface InitialState {
     config: TServiceTypeSettings[],
@@ -32,13 +8,13 @@ interface InitialState {
 }
 
 const initialState: InitialState = {
-    config: initialData,
+    config: [],
     isLoading: false,
 }
 
 export const bookingFlowConfigReducer = createReducer(initialState, builder => builder
     .addCase(setBookingFlowConfig, (state, {payload}) => {
-        return {...state, config: payload?.length ? payload : initialData};
+        return {...state, config: payload};
     })
     .addCase(setBookingFlowConfigLoading, (state, {payload}) => {
         return {...state, isLoading: payload};
