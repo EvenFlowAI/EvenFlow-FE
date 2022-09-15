b2a662b08b70d5ff448a7eba994cc7e2a07298d5
Alisa44 <alisa444.86@gmail.com>
2022-09-15 14:07:14 +0200

    widget embeded

--- src/components/Admin/Reporting/Reporting.tsx
+++ src/components/Admin/Reporting/Reporting.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {Titles} from "../../../config/constants";
 import {TitleContainer} from "../../Content/TitleContainer/TitleContainer";
+import {IEndUserConfig} from "../../../qrveyEndUser/types";
+import QrveyEndUser from "../../../qrveyEndUser/QrvayEndUser";
 
 const Reporting = () => {
+    const configObject: IEndUserConfig = {
+        domain: 'https://pcuxl.qrveyapp.com',
+        appid: 'jjaR5hX2q',
+        apikey: '0nv7hbDaQmjdTbBLWGV81ldOgX9QGLmKGYH3t6Dt',
+    };
     return (
-        <div>
+        <div style={{display: "block", width: "100%"}}>
             <TitleContainer
                 title={Titles.Reporting}
                 pad/>
-
+            <QrveyEndUser settings={configObject}/>
         </div>
     );
 };
