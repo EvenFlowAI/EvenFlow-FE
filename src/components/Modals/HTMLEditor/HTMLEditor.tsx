bfefd11eebc81e8a38b14f14b57fe4af620a0904
Alisa44 <alisa444.86@gmail.com>
2022-09-20 17:12:54 +0200

    HTML EDitor

---
+++ src/components/Modals/HTMLEditor/HTMLEditor.tsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import {BaseModal, DialogActions, DialogContent, DialogTitle} from "../BaseModal";
+import {DialogProps} from "../types";
+import {Editor} from "react-draft-wysiwyg";
+import {EditorState} from "draft-js";
+import {convertToHTML} from "draft-convert";
+import {Button} from "@material-ui/core";
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import {LoadingButton} from "../../UI/Button";
+import {makeStyles} from "@material-ui/core/styles";
+import classnames from 'classnames';
+
+type THTMLEditor = DialogProps & {
+    onSave: (value: string) => void;
+    isLoading?: boolean;
+}
+
+const useStyles = makeStyles(({
+    editor: {
+        border: '1px solid #F1F1F1',
+    }
+}))
+
+const HtmlEditor: React.FC<THTMLEditor> = ({open, onClose, title, onSave, isLoading}) => {
+    const [editorState, setEditorState] = useState<EditorState>(EditorState.createEmpty())
+    const styles = useStyles();
+
+    const onEditorStateChange = (value: EditorState) => {
+        setEditorState(value)
+    };
+
+    const onCancel = () => onClose();
+
+    const onSubmit = () => onSave(convertToHTML(editorState.getCurrentContent()))
+
+    return (
+        <BaseModal onClose={onCancel} open={open}>
+            <DialogTitle onClose={onCancel}>{title}</DialogTitle>
+            <DialogContent>
+                <div>
+                    <Editor
+                        placeholder="Type Here..."
+                        editorState={editorState}
+                        wrapperClassName="wrapper-class"
+                        editorClassName={classnames("editor-class", styles.editor)}
+                        toolbarClassName="toolbar-class"
+                        onEditorStateChange={onEditorStateChange}
+                    />
+                </div>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" onClick={onCancel} color="primary">
+                    Cancel
+                </Button>
+                <LoadingButton variant="contained" onClick={onSubmit} loading={isLoading}>
+                    Save
+                </LoadingButton>
+            </DialogActions>
+        </BaseModal>
+    );
+};
+
+export default HtmlEditor;
