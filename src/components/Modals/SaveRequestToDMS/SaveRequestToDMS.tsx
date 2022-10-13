bf608bd92e0a8c0a08eca1d7047beb6172459130
Alisa44 <alisa444.86@gmail.com>
2022-10-13 10:14:29 +0200

    changed sorting in Save to DMS window

--- src/components/Modals/SaveRequestToDMS/SaveRequestToDMS.tsx
+++ src/components/Modals/SaveRequestToDMS/SaveRequestToDMS.tsx
@@ -229,7 +229,10 @@ const SaveRequestToDms: React.FC<TSaveRequestModalProps> = ({ packageData, setPa
                             </TableHead>
                             <TableBody>
                                 <TableRow className={classes.emptyRow} key="empty"/>
-                                {newRequests.map((request, rowIndex) => {
+                                {newRequests
+                                    .slice()
+                                    .sort((a, b) => a.orderIndex - b.orderIndex)
+                                    .map((request, rowIndex) => {
                                     return <TableRow className={rowIndex % 2 === 0 ?  classes.row : classes.rowGrey} key={request.code}>
                                         <TableCell className={classes.requestCell} key={request.description}>{request.description}</TableCell>
                                         {temporaryData?.options.map((option, cellIndex) => {
