469ce21e11905f29af3b4a19675bcca30976a836
Alisa44 <alisa444.86@gmail.com>
2022-12-08 13:02:15 +0100

    transportation completed

--- src/components/Modals/CreateEmployee/CreateEmployee.tsx
+++ src/components/Modals/CreateEmployee/CreateEmployee.tsx
@@ -155,7 +155,7 @@ export const CreateEmployee: React.FC<DialogProps<IEmployee>> = ({payload, onAct
             if (!technicianForm.phoneNumber?.length) {
                 err = [...err, '"Phone Number" must not be empty'];
             } else {
-                if (technicianForm.phoneNumber.length < 11) err = [...err, '"Phone Number is not valid"']
+                if (technicianForm.phoneNumber.length < 11) err = [...err, '"Phone Number" is not valid']
             }
             if (technicianForm.email?.length) {
                 if (!checkEmail(technicianForm.email)) err = [...err, '"Email" is not valid']
@@ -169,7 +169,7 @@ export const CreateEmployee: React.FC<DialogProps<IEmployee>> = ({payload, onAct
             if (!advisorForm.phoneNumber?.length) {
                 err = [...err, '"Phone Number" must not be empty'];
             } else {
-                if (advisorForm.phoneNumber.length < 11) err = [...err, '"Phone Number is not valid"']
+                if (advisorForm.phoneNumber.length < 11) err = [...err, '"Phone Number" is not valid']
             }
             if (!advisorForm.email?.length) {
                 err = [...err, '"Email" must not be empty'];
