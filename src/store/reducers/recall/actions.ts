1f311bc8c03eddaf1715434763a96991dc1c6245
Alisa44 <alisa444.86@gmail.com>
2022-12-09 12:57:15 +0100

    validation recall modal

--- src/store/reducers/recall/actions.ts
+++ src/store/reducers/recall/actions.ts
@@ -1,8 +1,14 @@
 import {createAction} from "@reduxjs/toolkit";
-import {IRecall} from "./types";
+import {ICreateUpdateRecall, IRecall} from "./types";
 import {AppThunk} from "../../../types/types";
 
 export const getRecalls  = createAction<IRecall[]>('Recall/GetRecalls');
 
 export const loadRecalls = (): AppThunk => dispatch => {
+}
+
+export const createRecall = (data: ICreateUpdateRecall, onError: (err: string) => void): AppThunk => dispatch => {
+}
+
+export const updateRecall = (data: ICreateUpdateRecall, onError: (err: string) => void): AppThunk => dispatch => {
 }
--- src/store/reducers/recall/types.ts
+++ src/store/reducers/recall/types.ts
@@ -1,14 +1,32 @@
+export type TMakeModel = {
+    id: number;
+    name: string;
+}
+
 export interface IRecall {
     id: number;
     recallCampaignNumber: string;
-    make: string;
-    model: string;
+    make: TMakeModel;
+    model: TMakeModel;
     year: number;
     recallComponent: string;
     recallSummary: string;
     partLeadDaysCount: number;
-    dailyPartsCount: string;
+    dailyPartsCount: number;
     serviceRequestId: number;
     serviceRequestCode: string;
     serviceCenterId: number;
+}
+
+export interface ICreateUpdateRecall {
+    id?: number;
+    recallCampaignNumber: string;
+    makeId: number|null;
+    modelId: number|null;
+    year: number;
+    recallComponent: string;
+    recallSummary: string;
+    partLeadDaysCount: number;
+    dailyPartsCount: number;
+    serviceRequestId: number|null;
 }
