4cefd8f462c2423505edd270fa95c78829aad993
Alisa44 <alisa444.86@gmail.com>
2023-03-24 14:19:18 +0100

    package price eliminated if there is zero value

--- src/components/AppointmentFlow/AppointmentFrame/PackageSelectionParts/Total.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/PackageSelectionParts/Total.tsx
@@ -32,10 +32,12 @@ const Total: React.FC<TTotalProps> = ({ isBmWService, packages, handleClick, isS
                     <div className="before">
                       ${scProfile?.isRoundPrice ? priceBefore : priceBefore.toFixed(2)}
                     </div> }
-                    <div className="currentWrp">
-                        <div className="triangle"/>
-                        <div className="current">${scProfile?.isRoundPrice ? p.price : p.price.toFixed(2)}</div>
-                    </div>
+                    {p.price
+                        ? <div className="currentWrp">
+                            <div className="triangle"/>
+                            <div className="current">${scProfile?.isRoundPrice ? p.price : p.price.toFixed(2)}</div>
+                        </div>
+                    : null}
                 </div>
             }
         )}
