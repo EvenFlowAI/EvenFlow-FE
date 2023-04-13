8af92e8aa97931c4a0fbbdfee3a5eb34c56e7d22
Alisa44 <alisa444.86@gmail.com>
2023-04-13 15:26:40 +0200

    step fixed

--- src/components/AppointmentFlow/AppointmentFrame/SideBar.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/SideBar.tsx
@@ -4,7 +4,7 @@ import {TScreen} from "../../Layout/types";
 import {ProgressStepper} from "../ProgressStepper";
 import {
     setAdditionalServicesChosen, setSideBarActualSteps, setSideBarMenu,
-    setSideBarSteps
+    setSideBarSteps, setSideBarStepsList
 } from "../../../store/reducers/appointmentFrameReducer/actions";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../store/rootReducer";
@@ -67,7 +67,7 @@ type TProps = {
 }
 
 export const SideBar: React.FC<TProps> = ({screen, handleSetScreen}) => {
-    const {serviceType, sideBarSteps, sideBarMenu, sideBarActualSteps} = useSelector((state: RootState) => state.appointmentFrame);
+    const {serviceType, sideBarSteps, sideBarMenu, sideBarActualSteps, sideBarStepsList} = useSelector((state: RootState) => state.appointmentFrame);
     const {config} = useSelector((state: RootState) => state.bookingFlowConfig);
     const theme = useTheme();
     const dispatch = useDispatch();
@@ -87,6 +87,7 @@ export const SideBar: React.FC<TProps> = ({screen, handleSetScreen}) => {
 
     useEffect(() => {
         dispatch(setSideBarActualSteps(getStepsMap(serviceType, advisorSelection, appointmentSelection, transportationNeeds)))
+        dispatch(setSideBarStepsList(getStepsScreen(serviceType, advisorSelection, appointmentSelection, transportationNeeds)))
     }, [serviceType, advisorSelection, appointmentSelection, transportationNeeds, getStepsMap])
 
     useEffect(() => {
@@ -98,9 +99,10 @@ export const SideBar: React.FC<TProps> = ({screen, handleSetScreen}) => {
     }, [sideBarActualSteps, screen])
 
     const onClick = (idx: number) => {
-        const screen: TScreen = getStepsScreen(serviceType, advisorSelection, appointmentSelection, transportationNeeds)[idx];
-        handleSetScreen(screen);
-        if (idx === 0) dispatch(setAdditionalServicesChosen(true));
+        if (sideBarStepsList) {
+            handleSetScreen(sideBarStepsList[idx]);
+            if (idx === 0) dispatch(setAdditionalServicesChosen(true));
+        }
     }
 
     const getButtonState = useCallback((index: number) => {
