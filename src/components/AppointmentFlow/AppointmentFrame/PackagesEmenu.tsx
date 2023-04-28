d2f385a461437aeae13279a7399ffdbe69cf1541
Alisa44 <alisa444.86@gmail.com>
2023-04-28 11:39:03 +0200

    a file rendered

--- src/components/AppointmentFlow/AppointmentFrame/PackagesEmenu.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/PackagesEmenu.tsx
@@ -3,26 +3,18 @@ import {Loading} from "../../UI/Loading";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../store/rootReducer";
 import axios from "axios";
+import {useException} from "../../../utils/hooks";
 
 const PackagesEmenu = () => {
     const {selectedVehicle, makes} = useSelector((state: RootState) => state.appointmentFrame);
     const {scProfile} = useSelector((state: RootState) => state.appointment);
     const [isLoading, setLoading] = useState<boolean>(false);
     const [srcLink, setSrcLink] = useState<string>('');
+    const showError = useException();
 
     useEffect(() => {
-
-        axios.get('https://prodfix.emenusautomotive.com/my/index.php?go=api.pdf&vin=JTDEPRAEXLJ020044&mileage_service_type=30000M1',
-            {headers: { 'Subscription-Id': "ev99999"}})
-            .then(res => {
-                console.log(res)
-    }).catch(err => {
-            console.log(err)
-        })
-        let str1 = 'https://prodfix.emenusautomotive.com/my/index.php?go=api.pdf&';
-
         let str = 'https://prodfix.emenusautomotive.com/my/index.php?go=api.pdf&';
-        //let str = 'http://www.emenusllc.com/index.php?go=member.pdfTH&pdfType=threeUp&service_type=1&';
+        setLoading(true);
         if (selectedVehicle?.model) {
             if (selectedVehicle.vin?.length === 16) {
                 str = str + `&vin=${selectedVehicle.vin}`
@@ -33,20 +25,29 @@ const PackagesEmenu = () => {
                 if (selectedVehicle.year) str = str + `year=${selectedVehicle.year}&`;
             }
 
-            //if (scProfile?.dmsId) str = str + `member=${scProfile.dmsId}&`;
-
             if (selectedVehicle.mileage) str = str + `mileage_service_type=${selectedVehicle.mileage}TC`;
 
-            setSrcLink(str);
+            axios.get(str, {headers: { 'Subscription-Id': "ev99999"}, responseType: "arraybuffer"})
+                .then(res => {
+                    if (res.data) {
+                        console.log(res.data)
+                        const file = new Blob([res.data], {type: 'application/pdf'});
+                        console.log(file);
+                        const fileURL = URL.createObjectURL(file);
+                        setSrcLink(fileURL)
+                    }
+                })
+                .catch(err => {
+                    showError(err)
+                    console.log(err)
+                })
+                .finally(() => setLoading(false));
         }
     }, [selectedVehicle, scProfile])
-    // todo delete mock src
 
-    const src = 'http://www.emenusllc.com/index.php?go=member.pdfTH&pdfType=threeUp&service_type=1&model=43&member=2437&year=2010&mileage_service_type=30000TC';
     return isLoading
         ? <Loading/>
-        : <iframe src={srcLink} width="100%" style={{height: '50vh'}} id="e-menu">
-        </iframe>;
+        : <iframe src={srcLink} width="100%" style={{height: '50vh'}} id="e-menu"/>;
 };
 
 export default PackagesEmenu;
