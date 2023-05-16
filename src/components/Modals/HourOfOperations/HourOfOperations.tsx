1aee4618d807efee60af6fa3f6842b490fa38fc2
Alisa44 <alisa444.86@gmail.com>
2023-05-16 13:09:37 +0200

    hours of operations

--- src/components/Modals/HourOfOperations/HourOfOperations.tsx
+++ src/components/Modals/HourOfOperations/HourOfOperations.tsx
@@ -165,6 +165,7 @@ export const HourOfOperations: React.FC<DialogProps&TViewMode> = ({viewMode, ...
                     await Api.call(Api.endpoints.ServiceCenters.SetHOO, {data: {hoursOfOperations: fd}, urlParams: {id: selectedSC.id}});
                     setSaving(false);
                     showMessage("Hours of Operation updated");
+                    showMessage("The Unplanned Demand Settings for edited days were reset", "info");
                     props.onClose();
                 } catch (e) {
                     showError(e);
