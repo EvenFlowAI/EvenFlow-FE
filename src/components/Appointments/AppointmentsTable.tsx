1ae71697a77cd74ba98fed4077af372f9009b801
Alisa44 <alisa444.86@gmail.com>
2023-07-17 15:10:57 +0200

    EF-2984

--- src/components/Appointments/AppointmentsTable.tsx
+++ src/components/Appointments/AppointmentsTable.tsx
@@ -15,11 +15,11 @@ import {useSelector} from "react-redux";
 import {RootState} from "../../store/rootReducer";
 
 const cols: TableRowDataType<IAppointmentByQuery>[] = [
-    {header: "Date", val: el => moment.utc(el.dateInUtc).format("LL"), orderId: "date"},
-    {header: "Time", val: el => moment(el.timeSlot, timeSpanString).format(timeString)},
+    {header: "Date", val: el => el.dateInUtc ? moment.utc(el.dateInUtc).format("LL") : "", orderId: "date"},
+    {header: "Time", val: el => el.timeSlot ? moment(el.timeSlot, timeSpanString).format(timeString) : ""},
     {header: "Full Name", val: el => el.driver?.fullName ?? "", orderId: "fullName"},
     {header: "Car Info", val: el => `${el.vehicle?.make ?? ''} ${el.vehicle?.model ?? ''} ${el.vehicle?.year ?? ''}`},
-    {header: "Status", val: el => appointmentStatuses[el.appointmentStatus], orderId: "appointmentStatus"},
+    {header: "Status", val: el => el.appointmentStatus ? appointmentStatuses[el.appointmentStatus] : "", orderId: "appointmentStatus"},
     {header: "Job Type", val: el => typeof el.jobType !== 'undefined' && Number.isInteger(el.jobType) ? jobTypes[el.jobType] : ""},
 ]
 
