7b78e08dcdbef6e59b0ca3c4317abd8208de7a05
Yaroslav Ivasyuk <yaroslav.ivasyuk@gmail.com>
2023-07-25 13:35:19 +0300

    temporary dev host

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import {IOrder} from "../types/types";
 import {TRole} from "../store/reducers/users/types";
 
 //let apiHost = "https://be.dev.evenflow.ai";
-let apiHost = "https://3.134.209.56";
+let apiHost = "http://3.134.209.56:5000";
 
 // todo change
 // let apiHost = "https://api.evenflow.ai";
