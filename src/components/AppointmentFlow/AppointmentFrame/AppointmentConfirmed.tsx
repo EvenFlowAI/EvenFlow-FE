f8d4648d7ae1ca88ee2624713ec379aab4ba3388
Alisa44 <alisa444.86@gmail.com>
2023-08-23 09:59:47 +0200

    EF-3216

--- src/components/AppointmentFlow/AppointmentFrame/AppointmentConfirmed.tsx
+++ src/components/AppointmentFlow/AppointmentFrame/AppointmentConfirmed.tsx
@@ -367,6 +367,7 @@ export const AppointmentConfirmed: React.FC<TProps> = ({onUpdateAppointment}) =>
         if (vehicle) {
             if (customerLoadedData) {
                 await dispatch(setCustomerLoadedData({...customerLoadedData, isUpdating: true}))
+                await dispatch(clearAppointmentData())
             }
             await onUpdateAppointment(vehicle)
             await dispatch(setCurrentFrameScreen("manageAppointment"))
