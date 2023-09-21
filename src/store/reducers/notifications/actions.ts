3ef8b925429730859f4dbb7011a4c9ca70a1bb9b
Alisa44 <alisa444.86@gmail.com>
2023-09-21 15:11:46 +0200

    EF-1875 actions started

--- src/store/reducers/notifications/actions.ts
+++ src/store/reducers/notifications/actions.ts
@@ -1,6 +1,7 @@
 import {createAction} from "@reduxjs/toolkit";
+import {TPodNotifications, TSCNotifications} from "./types";
 
 export const setLoading = createAction<boolean>('Notifications/SetLoading');
-export const setSCNotifications = createAction<boolean>('Notifications/SetSCNotifications');
-export const setPodNotifications = createAction<boolean>('Notifications/SetPodNotifications');
-export const setRecallNotifications = createAction<boolean>('Notifications/SetRecallNotifications');
+export const setSCNotifications = createAction<TSCNotifications>('Notifications/SetSCNotifications');
+export const setPodNotifications = createAction<TPodNotifications>('Notifications/SetPodNotifications');
+export const setRecallNotifications = createAction<TSCNotifications>('Notifications/SetRecallNotifications');
--- src/store/reducers/notifications/reducer.ts
+++ src/store/reducers/notifications/reducer.ts
@@ -1,18 +1,38 @@
 import {createReducer} from "@reduxjs/toolkit";
 import {
-    setLoading,
+    setLoading, setSCNotifications,
 } from "./actions";
+import {TPodNotifications, TSCNotifications} from "./types";
 
 type TState = {
     isLoading: boolean;
+    scNotifications: TSCNotifications;
+    podNotifications: TPodNotifications;
+    recallNotifications: TSCNotifications;
+}
+
+const initialSCNotifications: TSCNotifications = {
+    isActive: false,
+    employeeIds: []
+}
+
+const podInitialNotifications: TPodNotifications = {
+    pod: null,
+    employeeIds: []
 }
 
 const initialState: TState = {
     isLoading: false,
+    scNotifications: initialSCNotifications,
+    podNotifications: podInitialNotifications,
+    recallNotifications: initialSCNotifications,
 }
 
 export const notificationsReducer = createReducer<TState>(initialState, builder => builder
     .addCase(setLoading, (state, {payload}) => {
         return {...state, isLoading: payload}
+    })
+    .addCase(setSCNotifications, (state, {payload}) => {
+        return {...state, scNotifications: payload}
     })
 )
