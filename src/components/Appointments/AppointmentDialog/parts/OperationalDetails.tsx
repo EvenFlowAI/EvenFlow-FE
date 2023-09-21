99c6fbc5f9cb7a39cb2abafef4a3b8426d11cee4
Alisa44 <alisa444.86@gmail.com>
2023-09-21 14:50:12 +0200

    EF-3414

--- src/components/Appointments/AppointmentDialog/parts/OperationalDetails.tsx
+++ src/components/Appointments/AppointmentDialog/parts/OperationalDetails.tsx
@@ -19,7 +19,7 @@ const OperationalDetails: React.FC<{payload: IAppointment}> = ({payload}) => {
             <div className={classes.blockTitle}>Operational Details</div>
             <DetailsItem title="Created Date" text={moment(payload.createdDateTime).format(dateTimeFormat)} key="date"/>
             <DetailsItem title="Scheduler" text={payload.scheduler.fullName ?? ''} key="Scheduler"/>
-            <DetailsItem title="Service Book details" text={payload.serviceBook?.name ?? ''} key="Service"/>
+            <DetailsItem title="Service Book" text={payload.serviceBook?.name ?? ''} key="Service"/>
             <DetailsItem title="Appointment Notes" text={payload.notes ?? ''} key="Appointment"/>
         </div>
     );
