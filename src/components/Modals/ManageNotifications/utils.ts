44a905f2c7ea03261545847486f6352e5c2a7ea9
Alisa44 <alisa444.86@gmail.com>
2023-09-27 10:54:26 +0200

    EF-1875 validation function refactored

--- src/components/Modals/ManageNotifications/utils.ts
+++ src/components/Modals/ManageNotifications/utils.ts
@@ -1,33 +1,46 @@
 import {TPodNotifications} from "../../../store/reducers/notifications/types";
 
-export const checkPodsAreTheSame = (localData: TPodNotifications[], remoteData: TPodNotifications[]): boolean => {
-    let changesAreSaved = true;
-    if (remoteData.length !== localData.length) {
-        changesAreSaved = false;
-    } else {
-        localData.forEach(podData => {
-            const podExistsLocally = localData.find(el => el.podId === podData.podId)
-            const podExistsRemotely = remoteData.find(el => el.podId === podData.podId)
-            if (!podExistsLocally || !podExistsRemotely) {
-                changesAreSaved = false;
-            } else {
-                const currentPod = remoteData.find(el => el.podId === podData.podId)
-                if (currentPod && podData.usersList) {
-                    if (currentPod.usersList?.length !== podData.usersList?.length) {
-                        changesAreSaved = false;
-                    } else {
-                        for (let userId of podData.usersList) {
-                            if (!currentPod.usersList?.includes(userId)) {
-                                changesAreSaved = false;
-                                return;
-                            }
-                        }
-                    }
-                } else {
-                    changesAreSaved = false;
-                }
-            }
+export function checkPodsAreTheSame(localData: TPodNotifications[], remoteData: TPodNotifications[]): boolean {
+    return (localData.length === remoteData.length)
+        &&
+        localData.every(localItem => {
+            return remoteData.find(remoteItem => {
+                return remoteItem.podId === localItem.podId
+                    && remoteItem.usersList?.length === localItem.usersList?.length
+                    && remoteItem.usersList?.every(remoteDataUsersList => localItem.usersList?.indexOf(remoteDataUsersList) !== -1);
+
+            });
         })
-    }
-    return changesAreSaved;
 }
+
+// export const checkPodsAreTheSame = (localData: TPodNotifications[], remoteData: TPodNotifications[]): boolean => {
+//     let changesAreSaved = true;
+//     if (remoteData.length !== localData.length) {
+//         changesAreSaved = false;
+//     } else {
+//         localData.forEach(podData => {
+//             const podExistsLocally = localData.find(el => el.podId === podData.podId)
+//             const podExistsRemotely = remoteData.find(el => el.podId === podData.podId)
+//             if (!podExistsLocally || !podExistsRemotely) {
+//                 changesAreSaved = false;
+//             } else {
+//                 const currentPod = remoteData.find(el => el.podId === podData.podId)
+//                 if (currentPod && podData.usersList) {
+//                     if (currentPod.usersList?.length !== podData.usersList?.length) {
+//                         changesAreSaved = false;
+//                     } else {
+//                         for (let userId of podData.usersList) {
+//                             if (!currentPod.usersList?.includes(userId)) {
+//                                 changesAreSaved = false;
+//                                 return;
+//                             }
+//                         }
+//                     }
+//                 } else {
+//                     changesAreSaved = false;
+//                 }
+//             }
+//         })
+//     }
+//     return changesAreSaved;
+// }
