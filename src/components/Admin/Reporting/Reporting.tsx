2dd81b0a4caef2e369ac586ffe2e3c6392f8de55
Alisa44 <alisa444.86@gmail.com>
2023-09-27 10:32:08 +0200

    reporting

--- src/components/Admin/Reporting/Reporting.tsx
+++ src/components/Admin/Reporting/Reporting.tsx
@@ -10,12 +10,13 @@ const Reporting = () => {
         appid: 'jjaR5hX2q',
         apikey: '0nv7hbDaQmjdTbBLWGV81ldOgX9QGLmKGYH3t6Dt',
     };
+
     return (
         <div style={{display: "block", width: "100%"}}>
             <TitleContainer
                 title={Titles.Reporting}
                 pad/>
-            {process.env.REACT_APP_ENV !== "production" ? <QrveyEndUser settings={configObject}/> : null}
+            <QrveyEndUser settings={configObject}/>
         </div>
     );
 };
