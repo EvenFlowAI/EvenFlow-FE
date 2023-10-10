48201eb48e8aaee13269a9e57f6879d21e8328b1
Alisa44 <alisa444.86@gmail.com>
2023-10-10 15:03:39 +0200

    EF-2856

--- src/components/Modals/AddServiceCategory/OpsCodesWithOrder.tsx
+++ src/components/Modals/AddServiceCategory/OpsCodesWithOrder.tsx
@@ -37,14 +37,16 @@ const OpsCodesWithOrder:React.FC<TOpsCodesTableProps> = ({
 
     const onSROrderChange = (id: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
         e.persist()
-        const elementToChange = selectedCodes.find(item => item.id === id)
-        if (elementToChange) {
-            const updated = {...elementToChange, orderIndex: e.target?.value ?? ''}
-            setSelectedCodes(prev => {
-                const filtered = prev.filter(item => item.id !== id);
-                return [...filtered, updated]
-            })
-            if (e.target?.value) setWrongOrderIndexes(prev => prev.filter(el => el !== +e.target?.value))
+        if (e?.target?.value.match(/^\d+$/)) {
+            const elementToChange = selectedCodes.find(item => item.id === id)
+            if (elementToChange) {
+                const updated = {...elementToChange, orderIndex: e.target?.value ?? ''}
+                setSelectedCodes(prev => {
+                    const filtered = prev.filter(item => item.id !== id);
+                    return [...filtered, updated]
+                })
+                if (e.target?.value) setWrongOrderIndexes(prev => prev.filter(el => el !== +e.target?.value))
+            }
         }
     }
 
