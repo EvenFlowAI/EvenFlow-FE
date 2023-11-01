396a8c829f40e759efd1f1387eb4d5c78cd9940e
Alisa44 <alisa444.86@gmail.com>
2023-11-01 11:35:36 +0100

    EF-3648

---
+++ src/store/reducers/reporting/actions.ts
@@ -0,0 +1,22 @@
+import {createAction} from "@reduxjs/toolkit";
+import {AppThunk} from "../../../types/types";
+import {Api} from "../../../config/requests";
+import {IEndUserConfig} from "../../../qrveyEndUser/types";
+
+export const getConfig  = createAction<IEndUserConfig>('Reporting/GetConfig');
+export const setLoading  = createAction<boolean>('Reporting/SetLoading');
+
+export const loadToken = (id: number): AppThunk => (dispatch, getState) => {
+    const {config} = getState().reporting;
+    dispatch(setLoading(true))
+    Api.call(Api.endpoints.Qrvey.GetToken, {data: {serviceCenterId: id}})
+        .then(result => {
+            dispatch(getConfig({...config, qv_token: result?.data?.token ?? ''}))
+        })
+        .catch(err => {
+            console.log('get qrvey token error', err)
+        })
+        .finally(() => {
+            dispatch(setLoading(false))
+        })
+}
