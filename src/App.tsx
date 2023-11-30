32d837149d62f442d0fffbf8911cd36f3d064682
Alisa44 <alisa444.86@gmail.com>
2023-11-30 16:59:56 +0100

    EF-3681

--- src/App.tsx
+++ src/App.tsx
@@ -38,12 +38,11 @@ const App = () => {
         const timeNow = moment();
         const timeToReload = moment().set('hour', 3).set('minute', 0).set("second", 0)
         if (timeNow.isAfter(timeToReload)) timeToReload.add(1, 'day')
-        //const difference = moment(timeToReload).diff(timeNow, "millisecond", true)
-        const difference = moment().add(1, 'minute').diff(timeNow, "millisecond", true)
+        const difference = moment(timeToReload).diff(timeNow, "millisecond", true)
         setTimeout(() => {
             localStorage.setItem('timestamp', moment().utc(true).toISOString())
             window.location.reload(true)
-        }, difference)
+        }, difference > 0 ? difference : 0)
     }, [])
 
     useEffect(() => {
