e6dc75edc46da7e11e670e0c236c6499f1807b96
Alisa44 <alisa444.86@gmail.com>
2024-02-02 14:07:24 +0100

    EF-4057 icons

--- src/features/booking/AppointmentReminders/AppointmentReminders.tsx
+++ src/features/booking/AppointmentReminders/AppointmentReminders.tsx
@@ -6,7 +6,9 @@ import {RootState} from "../../../store/rootReducer";
 import {setReminders} from "../../../store/reducers/appointmentFrameReducer/actions";
 import {EReminderType} from "../../../store/reducers/appointment/types";
 import {useTranslation} from "react-i18next";
-import {FlexGroup, StyledLabel} from "./styles";
+import {StyledLabel} from "./styles";
+import {ReactComponent as CheckboxIcon} from '../../../assets/img/checkbox_outlined.svg'
+import {ReactComponent as CheckboxEmptyIcon} from '../../../assets/img/checkbox_empty1.svg'
 import {Info} from "../AppointmentFlow/AppointmentConfirmation/styles";
 
 type TRemindersProps = {
@@ -22,6 +24,11 @@ export const AppointmentReminders: React.FC<TRemindersProps> = ({isEmailRequired
         const reminder= reminders.find(el => el.toString() === EReminderType.Email.toString());
         return typeof reminder !== "undefined"
     }, [reminders])
+
+    const textChecked = useMemo(() => scProfile?.isSendReminders && reminders.includes(EReminderType.Sms),
+        [scProfile, reminders])
+    const emailChecked = useMemo(() => scProfile?.isSendReminders && reminders.includes(EReminderType.Email),
+        [scProfile, reminders])
 
     useEffect(() => {
         if (!isEmailRequired && emailReminder && !customer?.email) {
@@ -44,12 +51,14 @@ export const AppointmentReminders: React.FC<TRemindersProps> = ({isEmailRequired
     return (
         <div>
             <AppointmentConfirmationTitle>{t("Reminders")}</AppointmentConfirmationTitle>
-            <FlexGroup>
+            <div>
                 <StyledLabel
                     label={t("Text confirmations & Reminders")}
                     disabled={!scProfile?.isSendReminders}
                     control={<Checkbox
-                        checked={scProfile?.isSendReminders && reminders.includes(EReminderType.Sms)}
+                        icon={<CheckboxEmptyIcon/>}
+                        checkedIcon={<CheckboxIcon/>}
+                        checked={textChecked}
                         onChange={handleChange(EReminderType.Sms)}
                         color="primary" />}
                 />
@@ -58,12 +67,14 @@ export const AppointmentReminders: React.FC<TRemindersProps> = ({isEmailRequired
                     label={t("E-mail reminders")}
                     control={<Checkbox
                         disabled={!scProfile?.isSendReminders || (!isEmailRequired && !customer?.email)}
-                        checked={scProfile?.isSendReminders && reminders.includes(EReminderType.Email)}
+                        checked={emailChecked}
+                        icon={<CheckboxEmptyIcon/>}
+                        checkedIcon={<CheckboxIcon/>}
                         onChange={handleChange(EReminderType.Email)}
                         color="primary" />}
                 />
                 <Info>{t("By using this service, you accept the terms of our Visitor Agreement")}.</Info>
-            </FlexGroup>
+            </div>
         </div>
     );
 };
--- src/features/booking/AppointmentReminders/styles.tsx
+++ src/features/booking/AppointmentReminders/styles.tsx
@@ -1,10 +1,4 @@
-import {FormControlLabel, FormGroup, styled} from "@material-ui/core";
-
-export const FlexGroup = styled(FormGroup)({
-    // display: "flex",
-    // flexDirection: "row",
-    // alignItems: "center",
-})
+import {FormControlLabel, styled} from "@material-ui/core";
 
 export const StyledLabel = styled(FormControlLabel)({
     textTransform: 'uppercase',
