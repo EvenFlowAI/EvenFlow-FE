5201c60cf9861d96921940b853530cdc8ee0cc1c
Alisa44 <alisa444.86@gmail.com>
2024-03-21 14:16:16 +0100

    EF-4214 checkmark deleted

--- src/features/booking/AppointmentFlow/MaintenancePackagesMobile/TabLabel/TabLabel.tsx
+++ src/features/booking/AppointmentFlow/MaintenancePackagesMobile/TabLabel/TabLabel.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import {usePackageMobileStyles} from "../../../../../hooks/styling/usePackageMobileStyles";
-import {Done} from "@mui/icons-material";
 
 type TTabLabelProps = {
     text: string;
-    isSelected: boolean;
 }
 
-export const TabLabel: React.FC<React.PropsWithChildren<React.PropsWithChildren<TTabLabelProps>>> = ({text, isSelected}) => {
+export const TabLabel: React.FC<React.PropsWithChildren<React.PropsWithChildren<TTabLabelProps>>> = ({text}) => {
     const { classes  } = usePackageMobileStyles();
-    return <div className={classes.iconWrapper}>
-        {/*{isSelected &&*/}
-        {/*<Done className={classes.icon} htmlColor={'white'}/>}*/}
-        {text}</div>
+    return <div className={classes.iconWrapper}>{text}</div>
 }
