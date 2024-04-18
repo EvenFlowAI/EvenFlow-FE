5ca55847ef01e05ebc4c483550585b07972e0ece
Alisa44 <alisa444.86@gmail.com>
2024-04-18 17:02:35 +0200

    EF-4456

--- src/features/admin/ScreenSettings/PriceDisplayModal/PriceDisplayModal.tsx
+++ src/features/admin/ScreenSettings/PriceDisplayModal/PriceDisplayModal.tsx
@@ -1,33 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BaseModal, DialogActions, DialogContent, DialogTitle} from "../../../../components/modals/BaseModal/BaseModal";
-import {Button} from "@mui/material";
+import {Button, FormControlLabel, Radio, RadioGroup} from "@mui/material";
 import {Loading} from "../../../../components/wrappers/Loading/Loading";
 import {DialogProps} from "../../../../components/modals/BaseModal/types";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../../store/rootReducer";
 import {useStyles} from "../styles";
+import {Label} from "./Label/Label";
+import {changeRoundPriceSetting} from "../../../../store/reducers/pricingSettings/actions";
+import {useException} from "../../../../hooks/useException/useException";
+import {useSCs} from "../../../../hooks/useSCs/useSCs";
 
 const PriceDisplayModal: React.FC<DialogProps> = (props) => {
     const {isRoundPriceLoading, roundPrice} = useSelector((state: RootState) => state.pricingSettings);
+    const [value, setValue] = useState<string>('decimal');
+    const showError = useException();
+    const {selectedSC} = useSCs();
+    const dispatch = useDispatch();
     const {classes} = useStyles();
 
+    useEffect(() => {
+        setValue(roundPrice ? 'round' : 'decimal');
+    }, [roundPrice])
+
     const onCancel = () => {
+        setValue(roundPrice ? 'round' : 'decimal')
         props.onClose()
     }
 
     const onSave = () => {
+        selectedSC && dispatch(changeRoundPriceSetting(selectedSC.id, value === 'round', showError))
+        props.onClose()
+    }
 
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
     }
 
     return (
-        <BaseModal {...props} width={700} onClose={onCancel}>
+        <BaseModal {...props} width={550} onClose={onCancel}>
             <DialogTitle onClose={onCancel}>
                 Price Display
             </DialogTitle>
             <DialogContent>
                 {isRoundPriceLoading
                     ? <Loading/>
-                    : <div/>}
+                    : <RadioGroup aria-label="position" name="position" value={value} onChange={onChange}>
+                        <FormControlLabel
+                            style={{marginBottom: 8}}
+                            value="round"
+                            control={<Radio color="primary"/>}
+                            label={<Label text="Display rounded prices"/>}
+                            labelPlacement="end"
+                        />
+                        <FormControlLabel
+                            value="decimal"
+                            control={<Radio color="primary"/>}
+                            label={<Label text="Display fractional prices"/>}
+                            labelPlacement="end"
+                        />
+                    </RadioGroup>}
             </DialogContent>
             <DialogActions>
                 <div className={classes.actionsWrapper}>
@@ -37,7 +69,7 @@ const PriceDisplayModal: React.FC<DialogProps> = (props) => {
                             color="info"
                             style={{marginRight: 20}}
                             onClick={onCancel}>
-                            Close
+                            Cancel
                         </Button>
                         <Button
                             variant="contained"
