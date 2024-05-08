d4f234b33beba39e850a6926a49ffd46a7a47d2a
Alisa44 <alisa444.86@gmail.com>
2024-05-08 09:59:07 +0200

    EF-4486 Day of Week tables

--- src/features/admin/PricingLevels/DayOfWeekTab.tsx
+++ src/features/admin/PricingLevels/DayOfWeekTab.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DayOfWeekTabs from "../VariableDemand/DayOfWeekTabs/DayOFWeekTabs";
+import DayOfWeekTables from "../VariableDemand/DayOfWeekTables/DayOfWeekTables";
 import {DayOfWeek} from "../VariableDemand/DayOfWeek/DayOfWeek";
 import {Box} from "@mui/material";
 
@@ -7,6 +7,6 @@ export const DayOfWeekTab = () => {
     return <div>
         <DayOfWeek/>
         <Box p={1.5}/>
-        <DayOfWeekTabs/>
+        <DayOfWeekTables/>
     </div>
 };
