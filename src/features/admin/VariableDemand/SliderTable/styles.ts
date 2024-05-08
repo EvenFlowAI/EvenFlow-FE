029dfe41a66e126f29c459b032d17fb4ec124550
Alisa44 <alisa444.86@gmail.com>
2024-05-08 14:23:11 +0200

    EF-4486

--- src/features/admin/VariableDemand/SliderTable/styles.ts
+++ src/features/admin/VariableDemand/SliderTable/styles.ts
@@ -5,28 +5,38 @@ export const ColorfulSlider = withStyles(ValueSlider, {
     root: {
         '&.Mui-disabled': {
             '& > .MuiSlider-track': {
-                background: "linear-gradient(90deg, green 0%, green 20%, orange 20%, orange 40%, red 40%)",
+                background: "linear-gradient(90deg, #5FA077 0%, #5FA077 20%, #FFA500 20%, #FFA500 40%, #F50057 40%)",
             }
         },
     },
     track: {
         '&.Mui-disabled': {
-            background: "linear-gradient(90deg, green 0%, green 20%, orange 20%, orange 40%, red 40%)",
+            background: "linear-gradient(90deg, #5FA077 0%, #5FA077 20%, #FFA500 20%, #FFA500 40%, #F50057 40%)",
         },
-        background: "linear-gradient(90deg, green 0%, green 20%, orange 20%, orange 40%, red 40%)",
+        background: "linear-gradient(90deg, #5FA077 0%, #5FA077 20%, #FFA500 20%, #FFA500 40%, #F50057 40%)",
     },
     rail: {
-        background: "linear-gradient(90deg, green 0%, green 20%, orange 20%, orange 40%, red 40%)",
+        background: "linear-gradient(90deg, #5FA077 0%, #5FA077 20%, #FFA500 20%, #FFA500 40%, #F50057 40%)",
         opacity: 1
     },
 });
 
 export const InvertedSlider = withStyles(ValueSlider, {
-    rail: {
-        background: "linear-gradient(-90deg, green 0%, green 20%, orange 20%, orange 40%, red 40%)",
-        opacity: 1
+    root: {
+        '&.Mui-disabled': {
+            '& > .MuiSlider-track': {
+                background: "linear-gradient(90deg, #F50057 0%, #F50057 60%, #FFA500 60%, #FFA500 80%, #5FA077 80%)",
+            }
+        },
     },
     track: {
-        background: "transparent"
-    }
+        '&.Mui-disabled': {
+            background: "linear-gradient(90deg, #F50057 0%, #F50057 60%, #FFA500 60%, #FFA500 80%, #5FA077 80%)",
+        },
+        background: "linear-gradient(90deg, #F50057 0%, #F50057 60%, #FFA500 60%, #FFA500 80%, #5FA077 80%)",
+    },
+    rail: {
+        background: "linear-gradient(90deg, #F50057 0%, #F50057 60%, #FFA500 60%, #FFA500 80%, #5FA077 80%)",
+        opacity: 1
+    },
 });
