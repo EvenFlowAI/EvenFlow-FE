91c09902402ae176cce236b003ac7819c8d026a7
Alisa44 <alisa444.86@gmail.com>
2024-05-24 12:16:11 +0200

    EF-4631

--- src/features/booking/AppointmentFlow/ServiceNeeds/ServiceCard/CardIcon/CardIcon.tsx
+++ src/features/booking/AppointmentFlow/ServiceNeeds/ServiceCard/CardIcon/CardIcon.tsx
@@ -1,8 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {ReactComponent as Icon} from "../../../../../../assets/img/oil-icon.svg";
 import DefaultIcon from "../../../../../../assets/img/oil-icon.svg";
-import axios from "axios";
-import {Loading} from "../../../../../../components/wrappers/Loading/Loading";
 
 type TProps = {
     iconPath?: string;
@@ -11,49 +9,24 @@ type TProps = {
 }
 
 const CardIcon: React.FC<TProps> = ({iconPath, isSM, active}) => {
-    const [icon, setIcon] = useState<string>('');
-    const [isLoading, setLoading] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (iconPath) {
-            setLoading(true);
-            axios.get(iconPath, {withCredentials: false})
-                .then(({ data }) => {
-                    setIcon(data)
-                })
-                .catch(e => {
-                    console.log(e, 'error loading icon')
-                })
-                .finally(() => setLoading(false))
-        }
-    }, [iconPath])
-
-    return isLoading
-        ? <Loading/>
-        : iconPath
-            ? isSM
-                ? <span
-                    className="cardIcon"
-                    style={{ filter: active ? "invert(100%)" : "unset"}}>
-                    <img src={iconPath} style={{width: 78, height: 78}} alt={"service_category_logo"}/>
+    return iconPath
+        ? <span
+            className="cardIcon"
+            style={{ filter: active ? "invert(100%)" : "unset"}}>
+                    <img
+                        src={iconPath}
+                        style={{width: isSM ? 78 : 110, height: isSM ? 78 : 110}}
+                        alt={"service_category_logo"}/>
                  </span>
-                : icon
-                    ? <span
-                        className="cardIcon"
-                    style={{ filter: active ? "invert(100%)" : "unset"}}
-                    dangerouslySetInnerHTML={{__html: icon}}/>
-                    : <span
-                        className="cardIcon"
-                        style={{ filter: active ? "invert(100%)" : "unset"}}>
-                    <img src={DefaultIcon} style={{width: 65, height: 65}} alt={"service_category_logo"}/>
-                </span>
-            : <span
-                className="cardIcon"
-                style={{ filter: active ? "invert(100%)" : "unset"}}>
+        : <span
+            className="cardIcon"
+            style={{ filter: active ? "invert(100%)" : "unset"}}>
                     { isSM
-                        ? <img src={DefaultIcon} style={{width: 65, height: 65}} alt={"service_category_logo"}/>
-                        : <Icon />
-                    }
+                        ? <img
+                            src={DefaultIcon}
+                            style={{width: 65, height: 65}}
+                            alt={"service_category_logo"}/>
+                        : <Icon />}
         </span>
 };
 
