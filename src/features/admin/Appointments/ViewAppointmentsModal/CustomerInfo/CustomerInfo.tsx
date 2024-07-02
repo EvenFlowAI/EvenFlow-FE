28f771fd85175340ad4d8ab0017c0a767d1fa3bd
Alisa44 <alisa444.86@gmail.com>
2024-07-02 10:20:12 +0200

    EF-3569

--- src/features/admin/Appointments/ViewAppointmentsModal/CustomerInfo/CustomerInfo.tsx
+++ src/features/admin/Appointments/ViewAppointmentsModal/CustomerInfo/CustomerInfo.tsx
@@ -8,15 +8,17 @@ export const CustomerInfo:React.FC<React.PropsWithChildren<React.PropsWithChildr
     return (
         <div>
             <TitleWrapper>Customer Information</TitleWrapper>
-            {payload.customerInformation?.fullName
-                ? <div style={{color: "#252733", fontSize: 14,}}>{payload.customerInformation?.fullName}</div>
-                : null}
             <DetailsItem
-                title={payload.customerInformation?.companyName ?? ''}
+                title={payload.customerInformation?.fullName ?? ''}
                 text={`${payload.customerInformation?.email ?? ''} ${payload.customerInformation?.phoneNumber ?? ''}`}/>
             {payload.customerInformation?.dmsId
                 ? <div style={{color: "#252733", fontSize: 14,}}>Customer ID: {payload.customerInformation?.dmsId}</div>
                 : null}
+            {payload.customerInformation?.companyName
+                ? <div style={{color: "#252733", fontSize: 14, marginTop: 12}}>Company Name:
+                    <span style={{color: "#858585", fontSize: 14}}> {payload.customerInformation?.companyName}</span>
+            </div>
+            : null }
             <Divider style={{marginBottom: 24}}/>
         </div>
     );
