a2598218d3f5c834664b0ed0e7a89e004b52cd4c
Alisa44 <alisa444.86@gmail.com>
2024-07-02 10:24:30 +0200

    EF-3569

--- src/features/admin/Appointments/ViewAppointmentsModal/CustomerInfo/CustomerInfo.tsx
+++ src/features/admin/Appointments/ViewAppointmentsModal/CustomerInfo/CustomerInfo.tsx
@@ -3,6 +3,7 @@ import {IAppointment} from "../../../../../api/types";
 import {TitleWrapper} from "../styles";
 import {DetailsItem} from "../DetailsItem/DetailsItem";
 import {Divider} from "@mui/material";
+import {Text, Title} from "./styles";
 
 export const CustomerInfo:React.FC<React.PropsWithChildren<React.PropsWithChildren<{payload: IAppointment}>>> = ({payload}) => {
     return (
@@ -12,12 +13,10 @@ export const CustomerInfo:React.FC<React.PropsWithChildren<React.PropsWithChildr
                 title={payload.customerInformation?.fullName ?? ''}
                 text={`${payload.customerInformation?.email ?? ''} ${payload.customerInformation?.phoneNumber ?? ''}`}/>
             {payload.customerInformation?.dmsId
-                ? <div style={{color: "#252733", fontSize: 14,}}>Customer ID: {payload.customerInformation?.dmsId}</div>
+                ? <Title>Customer ID: {payload.customerInformation?.dmsId}</Title>
                 : null}
             {payload.customerInformation?.companyName
-                ? <div style={{color: "#252733", fontSize: 14, marginTop: 12}}>Company Name:
-                    <span style={{color: "#858585", fontSize: 14}}> {payload.customerInformation?.companyName}</span>
-            </div>
+                ? <Title style={{marginTop: 12}}>Company Name:<Text> {payload.customerInformation?.companyName}</Text></Title>
             : null }
             <Divider style={{marginBottom: 24}}/>
         </div>
---
+++ src/features/admin/Appointments/ViewAppointmentsModal/CustomerInfo/styles.tsx
@@ -0,0 +1,11 @@
+import {styled} from "@mui/material";
+
+export const Title = styled("div")({
+    color: "#252733",
+    fontSize: 14,
+})
+
+export const Text = styled("span")({
+    color: "#858585",
+    fontSize: 14,
+})
