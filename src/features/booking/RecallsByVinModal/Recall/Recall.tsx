4c8c1274f8290e6a611593b9a27d6d96606e6298
Alisa44 <alisa444.86@gmail.com>
2024-10-23 14:19:19 +0200

    EF-5458

--- src/features/booking/RecallsByVinModal/Recall/Recall.tsx
+++ src/features/booking/RecallsByVinModal/Recall/Recall.tsx
@@ -1,7 +1,7 @@
 import React, {useMemo} from 'react';
 import {CustomSwitch, Label, useStyles} from "../styles";
 import dayjs from "dayjs";
-import {Divider} from "@mui/material";
+import {Divider, useMediaQuery, useTheme} from "@mui/material";
 import {Icon} from "../InfoIcon/InfoIcon";
 import {IRecallByVin, TArgCallback} from "../../../../types/types";
 import {useTranslation} from "react-i18next";
@@ -17,6 +17,8 @@ type TProps = {
 
 const Recall: React.FC<TProps> = ({item, recalls, onAddService, index}) => {
     const {recallsByVin} = useSelector((state: RootState) => state.recalls);
+    const theme = useTheme();
+    const isSm = useMediaQuery(theme.breakpoints.down('sm'));
 
     const {t} = useTranslation();
     const { classes  } = useStyles();
@@ -28,11 +30,11 @@ const Recall: React.FC<TProps> = ({item, recalls, onAddService, index}) => {
         : recalls.find(el => item.campaignNumber ? el.campaignNumber === item.campaignNumber : el.oemProgram === item.oemProgram)
             ? <>
                 {t("Service Added")}
-                {item.rolloverMessage?.length ? <Icon item={item}/> : null}
+                {!item.rolloverMessage?.length ? <Icon item={item}/> : null}
             </>
             : <>
                 {t("Service Declined")}
-                {item.rolloverMessage?.length ? <Icon item={item}/> : null}
+                {!item.rolloverMessage?.length ? <Icon item={item}/> : null}
             </>, [item, recalls, t])
 
     const checked = item.isRemedyAvailable && Boolean(recalls.find(el => {
@@ -60,22 +62,22 @@ const Recall: React.FC<TProps> = ({item, recalls, onAddService, index}) => {
                 </div>
                 <div className={classes.recallDetailsWrapper}>
                     {item.recallOpenDate
-                        ? <div>
+                        ? <div style={isSm ? {order: -1} : {}}>
                             <div className={classes.label}>{t("Recall Open Date")}</div>
                             <div className={classes.data}>
                                 {dayjs(item.recallOpenDate).format("MMM DD, YYYY")}
                             </div>
                         </div>
                         : null}
-                    <div>
+                    <div style={isSm ? {order: 2} : {}}>
                         <div className={classes.label}>{item.campaignNumber ? t("Campaign Number") : t("Manufacturer Program")}</div>
                         <div className={classes.data}>{item.campaignNumber ?? item.oemProgram ?? ''}</div>
                     </div>
-                    <div>
+                    <div style={isSm ? {order: 1} : {}}>
                         <div className={classes.label}>{t("Recall Component")}</div>
                         <div className={classes.data}>{item.recallComponent}</div>
                     </div>
-                    <div>
+                    <div style={isSm ? {order: 3} : {}}>
                         <div className={classes.label}>{t("Recall Status")}</div>
                         <div className={classes.data}
                              style={{color: 'red'}}>{item.recallStatus}</div>
