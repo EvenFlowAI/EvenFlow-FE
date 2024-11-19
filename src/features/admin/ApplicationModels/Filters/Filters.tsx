13320bcbdf8129d2a65523c587ef35a793f38b65
Alisa44 <alisa444.86@gmail.com>
2024-11-19 15:18:25 +0100

    EF-4762

--- src/features/admin/ApplicationModels/Filters/Filters.tsx
+++ src/features/admin/ApplicationModels/Filters/Filters.tsx
@@ -10,16 +10,17 @@ import {reviewOptions} from "../../../../utils/constants";
 
 type TProps = {
     onMakeChange: (e: React.ChangeEvent<{}>, option: IGlobalMake|null) => void;
-    onStatusChange: (e: React.ChangeEvent<{}>, option: TReviewOption) => void;
+    onStatusChange: (e: React.ChangeEvent<{}>, option: TReviewOption|null) => void;
     onModelsChange: (e: React.ChangeEvent<{}>, option: IGlobalModel[]) => void;
     modelsOptions: IGlobalModel[];
     isLoading: boolean;
-    selectedMake: any;
-    selectedModel: any;
-    selectedStatus: any;
+    selectedMake: IGlobalMake|null
+    selectedModel: IGlobalModel[];
+    selectedStatus: TReviewOption|null;
+    disabled: boolean;
 }
 
-const Filters: React.FC<TProps> = ({modelsOptions, selectedModel, onModelsChange, onMakeChange, onStatusChange, isLoading, selectedMake, selectedStatus}) => {
+const Filters: React.FC<TProps> = ({disabled, modelsOptions, selectedModel, onModelsChange, onMakeChange, onStatusChange, isLoading, selectedMake, selectedStatus}) => {
     const {allMakesOptions} = useSelector((state: RootState) => state.globalVehicles);
     const { classes  } = useAutocompleteStyles()
     return (
@@ -28,6 +29,7 @@ const Filters: React.FC<TProps> = ({modelsOptions, selectedModel, onModelsChange
                 style={{width: 180}}
                 loading={isLoading}
                 value={selectedMake}
+                disabled={disabled || isLoading}
                 options={allMakesOptions}
                 isOptionEqualToValue={(o, v) => o.id === v.id}
                 getOptionLabel={o => o.vinMake}
@@ -40,6 +42,7 @@ const Filters: React.FC<TProps> = ({modelsOptions, selectedModel, onModelsChange
             <Autocomplete
                 style={{width: 180}}
                 loading={isLoading}
+                disabled={disabled || isLoading}
                 value={selectedModel}
                 options={modelsOptions}
                 multiple
@@ -60,6 +63,7 @@ const Filters: React.FC<TProps> = ({modelsOptions, selectedModel, onModelsChange
                 isOptionEqualToValue={(o, v) => o === v}
                 getOptionLabel={o => o}
                 onChange={onStatusChange}
+                disabled={disabled || isLoading}
                 renderInput={autocompleteRender({
                     label: "Review Status",
                     placeholder: 'Not selected'
