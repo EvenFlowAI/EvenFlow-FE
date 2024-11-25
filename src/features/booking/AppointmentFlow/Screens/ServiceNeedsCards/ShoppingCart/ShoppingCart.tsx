0ff488225e9f9233fb8ea1decd640c5c3ede2642
Alisa44 <alisa444.86@gmail.com>
2024-11-25 11:59:58 +0100

    EF-5621 clean up

--- src/features/booking/AppointmentFlow/Screens/ServiceNeedsCards/ShoppingCart/ShoppingCart.tsx
+++ src/features/booking/AppointmentFlow/Screens/ServiceNeedsCards/ShoppingCart/ShoppingCart.tsx
@@ -79,7 +79,6 @@ const ShoppingCart = () => {
     }
 
     const deleteService = (item: IMaintenanceItem) => {
-        debugger
         switch (item.type) {
             case 'service':
                 dispatch(deleteIndService(item));
