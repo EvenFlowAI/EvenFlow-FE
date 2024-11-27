eb9860a23c311ad6db732c482ef69d7bc6888662
Alisa44 <alisa444.86@gmail.com>
2024-11-27 14:37:58 +0100

    EF-5633

--- src/features/booking/AppointmentFlow/Screens/Consultants/Consultants.tsx
+++ src/features/booking/AppointmentFlow/Screens/Consultants/Consultants.tsx
@@ -12,7 +12,6 @@ import {
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../../../store/rootReducer";
 import {Loading} from "../../../../../components/wrappers/Loading/Loading";
-import {EServiceCategoryType} from "../../../../../store/reducers/categories/types";
 import {useTranslation} from "react-i18next";
 import {
     getCurrentMenu,
@@ -21,7 +20,6 @@ import {
 } from "../../../utils/utils";
 import {useParams} from "react-router-dom";
 import {EServiceType} from "../../../../../store/reducers/appointmentFrameReducer/types";
-import {collectServiceRequestIds, mapRecallsForRequest} from "../../../../../utils/utils";
 import {ConsultantsWrapper} from "./styles";
 import {ConsultantCard} from "./ConsultantCard/ConsultantCard";
 import {TArgCallback} from "../../../../../types/types";
@@ -39,20 +37,15 @@ export const Consultants: React.FC<TProps> = ({isManagingFlow, handleNext, handl
         advisor: selectedConsultant,
         consultants,
         selectedPackage,
-        service,
-        subService,
         categoriesIds,
         selectedVehicle,
-        packagePricingType,
         serviceTypeOption,
-        packageEMenuType,
         isConsultantsLoading,
         selectedRecalls,
         address,
         zipCode,
     } = useSelector((state: RootState) => state.appointmentFrame);
     const {selectedSR, customerLoadedData} = useSelector((state: RootState) => state.appointment);
-    const {allCategories} = useSelector((state: RootState) => state.categories);
     const {mileage} = useSelector((state: RootState) => state.vehicleDetails);
     const {isAdvisorAvailable, isAppointmentTimingAvailable, isTransportationAvailable} = useSelector((state: RootState) => state.bookingFlowConfig);
     const dispatch = useDispatch();
@@ -60,21 +53,10 @@ export const Consultants: React.FC<TProps> = ({isManagingFlow, handleNext, handl
     const {t} = useTranslation();
 
     const serviceType = useMemo(() => serviceTypeOption ? serviceTypeOption.type : EServiceType.VisitCenter, [serviceTypeOption]);
-    const serviceRequestIds = useMemo(() => {
-        return collectServiceRequestIds(service, subService, null, selectedSR);
-    }, [service, subService, selectedSR]);
-
-    const getCategories = useCallback((): number[] => {
-        return allCategories
-            .filter(category => {
-                return category.type === EServiceCategoryType.GeneralCategory && categoriesIds.includes(category.id)
-            })
-            .map(item => item.id)
-    }, [allCategories, EServiceCategoryType, categoriesIds])
 
     const onCarIsValid = useCallback(() => {
         const someRequestsSelected = selectedSR.length || selectedPackage || categoriesIds.length || selectedRecalls.length;
-        const requestDataIsValid = serviceTypeOption?.type === EServiceType.VisitCenter || Boolean(address && zipCode)
+        const requestDataIsValid = serviceTypeOption?.type === EServiceType.VisitCenter || !serviceTypeOption || Boolean(address && zipCode)
         if (someRequestsSelected && requestDataIsValid && !customerLoadedData?.isUpdating) {
             dispatch(loadConsultants(id, serviceTypeOption?.id ?? null, onNext));
         }
@@ -83,10 +65,6 @@ export const Consultants: React.FC<TProps> = ({isManagingFlow, handleNext, handl
     useEffect(() => {
         dispatch(checkCarIsValid(onCarIsValid, undefined, true))
     }, [serviceTypeOption, id, selectedSR, selectedPackage, categoriesIds, selectedRecalls, selectedVehicle, mileage])
-
-    // useEffect(() => {
-    //     dispatch(loadConsultants(id, serviceTypeOption?.id ?? null, onNext))
-    // }, [id, serviceRequestIds, selectedVehicle, getCategories, mapRecallsForRequest, packageEMenuType, packagePricingType, selectedPackage, serviceTypeOption])
 
     useEffect(() => {
         dispatch(setSideBarMenu(getCurrentMenu(serviceType, isAdvisorAvailable, isTransportationAvailable, Boolean(isManagingFlow))))
