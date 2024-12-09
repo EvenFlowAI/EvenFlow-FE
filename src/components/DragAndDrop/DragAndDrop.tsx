15c86919eefa1af0c7febe11bc7809c2cafb83d1
Alisa44 <alisa444.86@gmail.com>
2024-12-09 15:59:52 +0100

    EF-5603 file structure

---
+++ src/components/DragAndDrop/DragAndDrop.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DndProvider } from 'react-dnd'
+
+const DragAndDrop = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DragAndDrop;
