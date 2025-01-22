77747cb1b7b98391e4af897eb9bdf4c806004a7c
Ivandev555 <ivan.danylevych@devabit.com>
2025-01-22 16:12:43 +0200

    cleanup

--- src/store/reducers/recall/reducer.ts
+++ src/store/reducers/recall/reducer.ts
@@ -43,7 +43,6 @@ export const recallsReducer = createReducer(initialState, builder =>
       return { ...state, recallPageData: { ...state.recallPageData, ...payload } };
     })
     .addCase(getRecallsByVin, (state, { payload }) => {
-      console.log('qwe', payload);
       return { ...state, recallsByVin: payload };
     })
     .addCase(setRecallOrder, (state, { payload }) => {
