b3b5fbbfa0d1389bc174f916f4e726291f3b48b0
Yaroslav Ivasiuk <yaroslav.ivasyuk@gmail.com>
2025-02-17 15:57:06 +0200

    added Dev and PreProd configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -16,6 +16,12 @@ switch (process.env.REACT_APP_ENV) {
   case 'local':
     apiHost = 'http://localhost:5000';
     break;
+  case "PreProd":
+    apiHost = "https://be.preprod.evenflow.ai";
+    break;
+  case 'Dev':
+    apiHost = 'https://be.dev.evenflow.ai';
+    break;
 }
 
 export const APIHost = apiHost;
