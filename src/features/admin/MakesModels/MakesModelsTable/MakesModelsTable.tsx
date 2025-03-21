fa159ea406afd3526e2774956073d685738fb8b5
Ivandev555 <ivan.danylevych@devabit.com>
2025-03-21 12:17:34 +0200

    EF-6042

--- src/features/admin/MakesModels/MakesModelsTable/MakesModelsTable.tsx
+++ src/features/admin/MakesModels/MakesModelsTable/MakesModelsTable.tsx
@@ -143,7 +143,7 @@ export const MakesModelsTable: React.FC<
         count={numberOfRecords}
       />
       <Menu open={Boolean(anchorEl)} onClose={onMenuClose} anchorEl={anchorEl}>
-        <MenuItem onClick={openEdit}>Edit Model</MenuItem>
+        <MenuItem onClick={openEdit}>Edit Models</MenuItem>
         {currentMake?.isReadOnly ? null : <MenuItem onClick={askRemove}>Remove</MenuItem>}
       </Menu>
     </div>
