7b1a9faefa6b2e791e14112f59c52bdb837c4ab6
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-06-25 10:58:45 +0300

    fix: EF-6519
    added styles for value and edit button in demand management table -> overbooking value

--- src/features/admin/UnplannedDemand/styles.ts
+++ src/features/admin/UnplannedDemand/styles.ts
@@ -7,4 +7,21 @@ export const useStyles = makeStyles()(() => ({
     justifyContent: 'center',
     gap: '34px',
   },
+  overBookingFactorWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '140px',
+    marginLeft: '52px',
+  },
+  overBookingValue: {
+    display: 'inline-block',
+    width: '60px',
+    textAlign: 'right',
+  },
+  editOverBooking: {
+    textTransform: 'none',
+    padding: 'unset',
+    minWidth: 'unset',
+  },
 }));
