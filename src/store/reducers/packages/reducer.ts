da8f928ac3f1522957e3a4c77831a162b7cb2ae7
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-07-23 10:08:47 +0300

    added new parameters for makes and models in maintenance packages
    refactor

--- src/store/reducers/packages/reducer.ts
+++ src/store/reducers/packages/reducer.ts
@@ -3,6 +3,7 @@ import {
   getAllComplimentary,
   getComplimentary,
   getMakes,
+  getMakesV2,
   getPackageById,
   getPackagesByQuery,
   getPackagesPaging,
@@ -24,6 +25,7 @@ const initialState: TState = {
   isPackageLoading: false,
   packages: [],
   makes: [],
+  makesV2: [],
   complimentary: [],
   isComplimentaryLoading: false,
   complimentaryPaging: {
@@ -55,6 +57,9 @@ export const packagesReducer = createReducer(initialState, builder =>
     })
     .addCase(getMakes, (state, { payload }) => {
       return { ...state, makes: payload };
+    })
+    .addCase(getMakesV2, (state, { payload }) => {
+      return { ...state, makesV2: payload };
     })
     .addCase(getComplimentary, (state, { payload }) => {
       return { ...state, complimentary: payload };
