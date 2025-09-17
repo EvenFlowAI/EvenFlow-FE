67101928d7d1237f772720c9d4a7e2009604a5e9
aborkovych <andrii.borkovych@devabit.com>
2025-09-17 13:22:56 +0300

    EF-6783: eMenu PDF components clean up (#3326)
    * EF-6534: Fix package reselection
    
    * EF-6772: Add package selection on back event (#3314)
    
    * EF-6783: eMenu PDF components clean up

--- src/store/reducers/appointment/types.ts
+++ src/store/reducers/appointment/types.ts
@@ -37,7 +37,6 @@ export interface IServiceCenterProfile {
   dmsId?: string;
   maintenancePackageOptionTypes: EMaintenanceOptionType[];
   packageSource: PackageSourceType;
-  eMenuPDF?: boolean;
   emailRequirement?: TEmailRequirement;
 }
 
