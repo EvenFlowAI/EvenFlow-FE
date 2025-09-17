43de462eb697132d7d089d3c8e13d0a8befc75e3
aborkovych <andrii.borkovych@devabit.com>
2025-09-17 11:19:26 +0300

    EF-1593: Fix error (#3325)

--- src/features/booking/AppointmentFlow/Screens/AppointmentTiming/constants.tsx
+++ src/features/booking/AppointmentFlow/Screens/AppointmentTiming/constants.tsx
@@ -1,27 +1,25 @@
 import { ReactComponent as SelectDateIcon } from '../../../../../assets/img/selectDateIcon.svg';
 import { ReactComponent as FirstAvailableIcon } from '../../../../../assets/img/firstAvailableIcon.svg';
 import { ReactComponent as OffersIcon } from '../../../../../assets/img/offersIcon.svg';
-import { TCard } from './types';
 import { EAppointmentTimingType } from '../../../../../store/reducers/appointment/types';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { TCard } from './types';
 
 export const timingTypes = ['Special Offers', 'Preferred Date', 'First Available Date'];
-const { t } = useTranslation();
 
-export const cards: TCard[] = [
+export const cardsConfig: TCard[] = [
   {
-    description: t('See appointments with special offer and shorter wait times'),
+    description: 'See appointments with special offer and shorter wait times',
     icon: <OffersIcon />,
     name: EAppointmentTimingType.SpecialOffers,
   },
   {
-    description: t('Choose a preferred date'),
+    description: 'Choose a preferred date',
     icon: <SelectDateIcon />,
     name: EAppointmentTimingType.PreferredDate,
   },
   {
-    description: t('Choose first available date'),
+    description: 'Choose first available date',
     icon: <FirstAvailableIcon />,
     name: EAppointmentTimingType.FirstAvailable,
   },
