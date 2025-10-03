16168f4f0b83a027c08aa93801bdefd95cc005df
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-10-03 16:58:51 +0300

    hide clone tag and add spaces to line | UAT

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -47,7 +47,7 @@ export const dealerShipAccessRoles: TRole[] = [
   Roles.DealerOwner,
   Roles.ServiceDirector,
   Roles.BDCManager,
-  Roles.BDCAgent
+  Roles.BDCAgent,
 ];
 
 export const timeSpanString = 'HH:mm:ss';
