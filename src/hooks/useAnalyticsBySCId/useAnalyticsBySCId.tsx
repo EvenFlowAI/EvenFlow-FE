f4d7636df50be6b5881e65b5d46a58947f4abf72
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-10-28 12:31:22 +0200

    EF-6717 added processing and getting client_id from dealers

--- src/hooks/useAnalyticsBySCId/useAnalyticsBySCId.tsx
+++ src/hooks/useAnalyticsBySCId/useAnalyticsBySCId.tsx
@@ -1,60 +1,36 @@
 import ReactGA from 'react-ga4';
 import TagManager from 'react-gtm-module';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { options } from '../../utils/constants';
 import { TReactGATracker } from '../../utils/types';
 import { TArgCallback } from '../../types/types';
 import { getTrackersForParentSite } from '../../utils/getTrackersForParentSite';
 import { getTrackerById } from '../../utils/getTrackerById';
 
-export const useAnalyticsBySCId = (
-  id: string,
-  trackerCreated: boolean,
-  setTrackerCreated: () => void
-) => {
-  function createTracker(opt_clientId = '', trackerCreated: boolean) {
-    const TRACKER = getTrackerById(id);
-    if (!trackerCreated) {
-      if (opt_clientId) options.clientId = opt_clientId;
-
-      ReactGA.initialize(TRACKER, {
-        gaOptions: options,
-      });
-      TagManager.initialize({
-        gtmId: TRACKER,
-      });
-      setTrackerCreated();
-    }
-  }
-
-  useEffect(() => {
-    if (!trackerCreated && id) {
-      createTracker('', trackerCreated);
-    }
-  }, [id]);
-
-  useEffect(() => {
-    trackerCreated &&
-      ReactGA.send({
-        hitType: 'pageview',
-        page: window.location.pathname + window.location.search,
-      });
-  }, [trackerCreated]);
-};
 export const useAnalyticsForParentSite = (
   id: string,
   trackerCreated: boolean,
   setTrackerCreated: TArgCallback<string[]>
 ) => {
+  const [clientId, setClientId] = useState<string | null>(null);
+
   function createTracker(opt_clientId = '', trackerCreated: boolean) {
     const TRACKERS = getTrackersForParentSite(id);
+
     if (!trackerCreated) {
       if (opt_clientId) options.clientId = opt_clientId;
+
       const trackersData: TReactGATracker[] = TRACKERS.map(el => ({
         trackingId: el.measurementId,
-        gaOptions: { ...options, name: el.measurementId },
+        gaOptions: {
+          ...options,
+          name: el.measurementId,
+        },
       }));
+
+      console.log('TEMP_LOG: initialization ga4');
       ReactGA.initialize(trackersData);
+
       TRACKERS.forEach(item => {
         if (item.gmtId) {
           TagManager.initialize({
@@ -62,15 +38,39 @@ export const useAnalyticsForParentSite = (
           });
         }
       });
+
       setTrackerCreated(trackersData.map(el => el.trackingId));
     }
   }
 
   useEffect(() => {
-    if (!trackerCreated && id) {
-      createTracker('', trackerCreated);
-    }
-  }, [id]);
+    const handleMessage = (event: MessageEvent) => {
+      const clientId = typeof event.data === 'string' ? event.data : '';
+
+      if (clientId) {
+        console.log('TEMP_LOG: client_id obtained from the dealer website:', clientId);
+        if (clientId?.length) setClientId(clientId);
+
+        if (!trackerCreated && id) {
+          createTracker(clientId, trackerCreated);
+        }
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, [trackerCreated, id]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!trackerCreated && id && clientId === null) {
+        createTracker('', trackerCreated);
+        console.log('TEMP_LOG: create tracker with no-client-id');
+      }
+    }, 3500);
+
+    return () => clearTimeout(timeout);
+  }, [id, trackerCreated, clientId]);
 
   useEffect(() => {
     trackerCreated &&
