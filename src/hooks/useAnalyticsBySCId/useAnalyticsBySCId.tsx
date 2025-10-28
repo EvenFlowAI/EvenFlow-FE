cf53776bdb3bc483bfcb82bd2db2e36bba0d96d7
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-10-28 14:45:48 +0200

    improved ga4

--- src/hooks/useAnalyticsBySCId/useAnalyticsBySCId.tsx
+++ src/hooks/useAnalyticsBySCId/useAnalyticsBySCId.tsx
@@ -12,13 +12,13 @@ export const useAnalyticsForParentSite = (
   trackerCreated: boolean,
   setTrackerCreated: TArgCallback<string[]>
 ) => {
-  const [clientId, setClientId] = useState<string | null>(null);
+  const [clientIdLocal, setClientId] = useState<string | null>(null);
 
   function createTracker(opt_clientId = '', trackerCreated: boolean) {
     const TRACKERS = getTrackersForParentSite(id);
 
     if (!trackerCreated) {
-      if (opt_clientId) options.clientId = opt_clientId;
+      if (opt_clientId) options.clientId = opt_clientId || (clientIdLocal ? clientIdLocal : '');
 
       const trackersData: TReactGATracker[] = TRACKERS.map(el => ({
         trackingId: el.measurementId,
@@ -49,28 +49,32 @@ export const useAnalyticsForParentSite = (
 
       if (clientId) {
         console.log('TEMP_LOG: client_id obtained from the dealer website:', clientId);
-        if (clientId?.length) setClientId(clientId);
+        if (clientId?.length) setClientId(clientId || clientIdLocal);
 
         if (!trackerCreated && id) {
-          createTracker(clientId, trackerCreated);
+          createTracker(clientId || '', trackerCreated);
+        }
+      } else {
+        if (!trackerCreated && id) {
+          createTracker(clientIdLocal || '', trackerCreated);
         }
       }
     };
 
     window.addEventListener('message', handleMessage);
     return () => window.removeEventListener('message', handleMessage);
-  }, [trackerCreated, id]);
+  }, [id, trackerCreated]);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (!trackerCreated && id && clientId === null) {
-        createTracker('', trackerCreated);
-        console.log('TEMP_LOG: create tracker with no-client-id');
-      }
-    }, 3500);
-
-    return () => clearTimeout(timeout);
-  }, [id, trackerCreated, clientId]);
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     if (!trackerCreated && id && clientId === null) {
+  //       createTracker('', trackerCreated);
+  //       console.log('TEMP_LOG: create tracker with no-client-id');
+  //     }
+  //   }, 3500);
+  //
+  //   return () => clearTimeout(timeout);
+  // }, [id, trackerCreated, clientId]);
 
   useEffect(() => {
     trackerCreated &&
