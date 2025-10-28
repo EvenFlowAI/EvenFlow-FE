be52324648bbad6496606d659532c3eb88284b50
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-10-28 15:00:06 +0200

    improved ga4 phase 3

--- src/hooks/useAnalyticsBySCId/useAnalyticsBySCId.tsx
+++ src/hooks/useAnalyticsBySCId/useAnalyticsBySCId.tsx
@@ -47,19 +47,18 @@ export const useAnalyticsForParentSite = (
     const handleMessage = (event: MessageEvent) => {
       const clientId = typeof event.data === 'string' ? event.data : '';
 
-      if (clientId) {
-        console.log('TEMP_LOG: client_id obtained from the dealer website:', clientId);
-        if (clientId?.length) setClientId(clientId || clientIdLocal);
-
-        if (!trackerCreated && id) {
-          createTracker(clientId || '', trackerCreated);
-        }
-      } else {
-        if (!trackerCreated && id) {
-          createTracker(clientIdLocal || '', trackerCreated);
-        }
-      }
+      console.log('TEMP_LOG: client_id obtained from the dealer website:', clientId);
+      if (clientId?.length) setClientId(clientId || clientIdLocal);
     };
+
+    if (clientIdLocal) {
+      console.log('TEMP_LOG: client_id obtained from the dealer website:', clientIdLocal);
+      // if (clientIdLocal?.length) setClientId( || clientIdLocal);
+
+      if (!trackerCreated && id) {
+        createTracker(clientIdLocal || '', trackerCreated);
+      }
+    }
 
     window.addEventListener('message', handleMessage);
   }, [id, trackerCreated]);
