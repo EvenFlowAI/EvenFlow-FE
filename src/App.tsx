d2bccd56b9a7c21ea4b4cb2c79b0e1ae7a185762
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-10-29 15:20:23 +0200

    Revert "fix 4"
    This reverts commit 2ec34339fb8d4153d96937280e83cc03716086b5.

--- src/App.tsx
+++ src/App.tsx
@@ -144,18 +144,20 @@ const App = () => {
     disableEmotionWarning();
   }, []);
 
-  const handleMessage = (event: MessageEvent) => {
-    console.log('222 clientId event received:', event.data);
-    const clientId = typeof event.data === 'string' ? event.data : '';
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      console.log('222 clientId event received:', event.data);
+      const clientId = typeof event.data === 'string' ? event.data : '';
 
-    if (clientId) {
-      console.log('222 TEMP_LOG: client_id obtained from the dealer website:', clientId);
-      if (clientId?.length) sessionStorage.setItem('clientId', clientId);
-    }
-  };
+      if (clientId) {
+        console.log('222 TEMP_LOG: client_id obtained from the dealer website:', clientId);
+        if (clientId?.length) sessionStorage.setItem('clientId', clientId);
+      }
+    };
 
-  window.addEventListener('message', handleMessage);
-  // return () => window.removeEventListener('message', handleMessage);
+    window.addEventListener('message', handleMessage);
+    // return () => window.removeEventListener('message', handleMessage);
+  }, []);
 
   const handleClose = (key: React.ReactText) => () => {
     notificationsRef?.current?.closeSnackbar(key);
