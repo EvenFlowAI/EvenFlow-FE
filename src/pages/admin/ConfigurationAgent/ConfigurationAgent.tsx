0faf64c7fffbeddca1de39b35d6b0c64539c42c1
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2025-12-12 17:22:38 +0200

    support updating service center and reload the agent

--- src/pages/admin/ConfigurationAgent/ConfigurationAgent.tsx
+++ src/pages/admin/ConfigurationAgent/ConfigurationAgent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAuthenticationTokenForAdmin } from '../../../api/helper';
 import { useSCs } from '../../../hooks/useSCs/useSCs';
 import Agent from '../../../features/admin/Agent/Agent';
@@ -11,22 +11,41 @@ const ConfigurationAgent = () => {
   const accessToken = getAuthenticationTokenForAdmin();
   const { classes } = useStyles();
   const iframe = document.getElementById('configuration-agent') as HTMLIFrameElement;
+  const [reloadKey, setReloadKey] = useState(0);
 
-  iframe?.addEventListener('load', () => {
-    if (accessToken?.length && selectedSC) {
+  const sendDataToAgent = () => {
+    if (selectedSC && iframe) {
       iframe.contentWindow?.postMessage(
         { scID: selectedSC.id, accessToken: accessToken },
         CONFIGURATION_AGENT_URL
       );
       console.log(`Message sent to iframe successfully with service center ID and access token.`);
     }
-  });
+  };
+
+  useEffect(() => {
+    if (iframe) {
+      iframe.addEventListener('load', () => {
+        if (accessToken?.length && selectedSC) {
+          sendDataToAgent();
+        }
+      });
+    }
+  }, [iframe]);
+
+  useEffect(() => {
+    if (selectedSC && iframe) {
+      sendDataToAgent();
+      setReloadKey(prev => prev + 1);
+    }
+  }, [selectedSC?.id]);
 
   return (
     <>
       <Agent agentName={'Configuration Agent'} />
       <div className={classes.wrapper}>
         <iframe
+          key={reloadKey}
           id="configuration-agent"
           src={CONFIGURATION_AGENT_URL}
           width="100%"
