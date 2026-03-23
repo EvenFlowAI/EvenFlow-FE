c879c37665bab06e917d24893dcf572114a851f4
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2026-03-23 10:27:51 +0200

    fix: EF-7582 simplify AddUserAccount modal dropdowns and dealership options handling
    - Replace redundant role condition in `AddUserAccountForm` with consistent `ServiceCenterCategoryDropdown` usage.
    - Adjust `AddDealershipGroupForm` to prioritize `accessibleDealerships` when `dealershipList` is empty and refine "Select all" logic.
    - Optimize `ServiceCenterCategoryDropdown` to handle single-category service centers without adding unnecessary "Select all" option.

--- src/components/modals/admin/AddUserAccount/Forms/AddDealershipGroupForm.tsx
+++ src/components/modals/admin/AddUserAccount/Forms/AddDealershipGroupForm.tsx
@@ -26,14 +26,22 @@ const AddDealershipGroupForm = ({
 }: DealershipGroupProps) => {
   const { classes: multipleACSClasses } = useMultipleACStyles();
 
-  const { dealershipList } = useSelector((state: RootState) => state.dealershipGroups);
+  const { dealershipList, accessibleDealerships } = useSelector(
+    (state: RootState) => state.dealershipGroups
+  );
 
-  const allOptions: TOption[] = dealershipList.map(d => ({
-    value: d.id,
-    name: d.name,
-  }));
+  const allOptions: TOption[] = dealershipList.length
+    ? dealershipList.map(d => ({
+        value: d.id,
+        name: d.name,
+      }))
+    : accessibleDealerships.map(d => ({
+        value: d.id,
+        name: d.name,
+      }));
 
-  const options: TOption[] = [{ value: 0, name: 'Select all' }, ...allOptions];
+  const options: TOption[] =
+    allOptions.length > 1 ? [{ value: 0, name: 'Select all' }, ...allOptions] : allOptions;
 
   const onCheckboxChange = useCallback(
     (option: TOption) => {
