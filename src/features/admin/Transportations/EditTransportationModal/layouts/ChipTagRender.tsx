fa1bd33bd3884e01284691f8be13415a313ce846
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2026-04-14 17:03:00 +0300

    feat: EF-7695 add ManufacturerForm component and integrate with RecallDatabaseFilters
    - Introduced `ManufacturerForm` to handle manufacturer selection with improved styling and functionality.
    - Replaced raw `Autocomplete` in `RecallDatabaseFilters` with the new `ManufacturerForm`.
    - Enhanced tag rendering via `renderChipTagsWithoutOptionObject` with improved visibility logic.
    - Added a helper function `calculateMaxVisibleTagsForWithoutOptionObject` for more precise tag display calculations.

--- src/features/admin/Transportations/EditTransportationModal/layouts/ChipTagRender.tsx
+++ src/features/admin/Transportations/EditTransportationModal/layouts/ChipTagRender.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Chip, Tooltip } from '@mui/material';
 import { TOption } from '../../types';
-import { calculateMaxVisibleTags, calculateMaxVisibleTagsForDealershipGroupForm } from '../helper';
+import {
+  calculateMaxVisibleTags,
+  calculateMaxVisibleTagsForDealershipGroupForm,
+  calculateMaxVisibleTagsForWithoutOptionObject,
+} from '../helper';
 
 const dayOrder: Record<string, number> = {
   Sunday: 0,
@@ -139,6 +143,84 @@ export const renderChipTagsForDealership = (
               <div>
                 {sortedValues.slice(maxVisibleTags).map(option => (
                   <div key={option.value}>{option.name}</div>
+                ))}
+              </div>
+            }
+            arrow
+            placement="top"
+          >
+            <Chip
+              key="others"
+              label={`+${remainingCount} others`}
+              size="medium"
+              color="primary"
+              variant="filled"
+              style={{
+                margin: '2px 4px 2px 0',
+                flexShrink: 0,
+              }}
+            />
+          </Tooltip>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export const renderChipTagsWithoutOptionObject = (
+  selectedValues: string[],
+  getTagProps: (params: { index: number }) => any,
+  containerWidth = 350,
+  handleDelete: (tag: string) => void
+) => {
+  const sortedValues = [...selectedValues].sort((a, b) => {
+    const aIndex = dayOrder[a] ?? 999;
+    const bIndex = dayOrder[b] ?? 999;
+    return aIndex - bIndex;
+  });
+
+  const maxVisibleTags = calculateMaxVisibleTagsForWithoutOptionObject(
+    sortedValues,
+    containerWidth
+  );
+  const visibleTags = sortedValues.slice(0, maxVisibleTags);
+  const remainingCount = sortedValues.length - maxVisibleTags;
+
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'nowrap',
+          overflow: 'hidden',
+        }}
+      >
+        {visibleTags.map((option, tagIndex) => {
+          const props = getTagProps({ index: tagIndex });
+          console.log(props.onDelete);
+          return (
+            <Chip
+              key={option}
+              label={option}
+              onDelete={() => handleDelete(option)}
+              size="medium"
+              color="primary"
+              variant="filled"
+              style={{
+                margin: '2px 4px 2px 0',
+                flexShrink: 0,
+                maxWidth: '130px',
+              }}
+              {...props}
+            />
+          );
+        })}
+        {remainingCount > 0 && (
+          <Tooltip
+            title={
+              <div>
+                {sortedValues.slice(maxVisibleTags).map(option => (
+                  <div key={option}>{option}</div>
                 ))}
               </div>
             }
--- src/pages/admin/RecallDatabase/RecallDatabaseFilters.tsx
+++ src/pages/admin/RecallDatabase/RecallDatabaseFilters.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { TextField } from '../../../components/formControls/TextFieldStyled/TextField';
 import { Search } from '@mui/icons-material';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../store/rootReducer';
-import { autocompleteRender } from '../../../utils/autocompleteRenders';
-import { Autocomplete } from '@mui/material';
-import { useAutocompleteStyles } from '../../../hooks/styling/useAutocompleteStyles';
 import dayjs from 'dayjs';
 import CustomDateRangePicker from '../../../components/pickers/CustomDateRangePicker/CustomDateRangePicker';
+import ManufacturerForm from './ManufacturerForm';
 
 interface RecallDatabaseFiltersProps {
   searchTerm: string;
@@ -26,32 +22,16 @@ const RecallDatabaseFilters = ({
   date,
   setDate,
 }: RecallDatabaseFiltersProps) => {
-  const { manufacturers } = useSelector((state: RootState) => state.recallDatabase);
-  const { classes } = useAutocompleteStyles();
-
   return (
     <div style={{ display: 'flex', gap: 12, alignItems: 'end' }}>
       <TextField
-        style={{ width: 182 }}
+        style={{ width: 320 }}
         placeholder="Search..."
         endAdornment={<Search />}
         value={searchTerm}
         onChange={e => setSearchTerm(e.target.value)}
       />
-      <Autocomplete
-        classes={classes}
-        style={{ width: 350 }}
-        multiple
-        value={manufacturer}
-        options={manufacturers}
-        isOptionEqualToValue={(o, v) => o === v}
-        getOptionLabel={o => o}
-        onChange={(e, v) => setManufacturer(v || '')}
-        renderInput={autocompleteRender({
-          label: 'Manufacturer',
-          placeholder: 'Not selected',
-        })}
-      />
+      <ManufacturerForm setManufacturer={setManufacturer} selectedManufacturer={manufacturer} />
       <CustomDateRangePicker
         shortcuts
         title="Date Reported"
