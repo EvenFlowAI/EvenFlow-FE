877dff4eafa5989dacd536dd03252028997b9197
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2026-05-06 17:45:00 +0300

    feat: EF-7819 support query param handling in booking flow
    - Added `URLQueryType` constants for easier management of query param keys.
    - Enhanced various booking components to read and utilize query params for pre-filling customer data and service center selection.
    - Improved URL management by encoding/decoding service center IDs and clearing query params post-processing.

--- src/features/booking/ServiceCenterSelect/ServiceCenterSelect.tsx
+++ src/features/booking/ServiceCenterSelect/ServiceCenterSelect.tsx
@@ -5,16 +5,37 @@ import { RootState } from '../../../store/rootReducer';
 import { IServiceCenter } from '../../../store/reducers/serviceCenters/types';
 import { TRole } from '../../../store/reducers/users/types';
 import {
+  clearAppointmentData,
+  setAddress,
+  setServiceOptionChanged,
+  setServiceTypeOption,
+  setSideBarSteps,
   setTrackerCreated,
+  setVehicle,
   setWelcomeScreenView,
+  setZipCode,
 } from '../../../store/reducers/appointmentFrameReducer/actions';
-import { NavLink, useParams } from 'react-router-dom';
+import { NavLink, useHistory, useParams } from 'react-router-dom';
 import { useStyles } from './styles';
 import { ServiceCenterCard } from './ServiceCenterCard/ServiceCenterCard';
 import { useCurrentUser } from '../../../hooks/useCurrentUser/useCurrentUser';
 import { Routes } from '../../../routes/constants';
 import { Roles } from '../../../types/types';
-import { decodeSCID } from '../../../utils/utils';
+import { decodeSCID, encodeSCID } from '../../../utils/utils';
+import {
+  loadSCProfile,
+  setCustomerEnteredEmail,
+  setCustomerLoadedData,
+} from '../../../store/reducers/appointment/actions';
+import { setCustomerSearchData } from '../../../store/reducers/enhancedCustomerSearch/actions';
+import { initialCustomerSearch } from '../../../store/reducers/constants';
+import {
+  FIRST_NAME,
+  LAST_NAME,
+  CONTACT,
+  SERVICE_CENTER_ID,
+  VIN,
+} from '../../../types/URLQueryType';
 
 const restrictedRoles: TRole[] = [
   Roles.ServiceManager,
@@ -30,6 +51,13 @@ const ServiceCenterSelect = () => {
   const { shortSC, shortLoading } = useSelector((state: RootState) => state.serviceCenters);
   const [centersList, setCentersList] = useState<IServiceCenter[]>([]);
   const { id } = useParams<{ id: string }>();
+  const params = new URL(window.location.href).searchParams;
+  const serviceCenterIdFromParams = params.get(SERVICE_CENTER_ID);
+  const contactFromParams = params.get(CONTACT);
+  const firstNameFromParams = params.get(FIRST_NAME);
+  const lastNameFromParams = params.get(LAST_NAME);
+  const vinCodeFromParams = params.get(VIN);
+  const history = useHistory();
 
   const { classes } = useStyles();
   const currentUser = useCurrentUser();
@@ -37,11 +65,20 @@ const ServiceCenterSelect = () => {
 
   useEffect(() => {
     if (shortSC?.length && currentUser) {
-      setCentersList(() =>
-        restrictedRoles.includes(currentUser?.role)
-          ? shortSC.filter(item => item.id === decodeSCID(id))
-          : shortSC
-      );
+      const scList = restrictedRoles.includes(currentUser?.role)
+        ? shortSC.filter(item => item.id === decodeSCID(id))
+        : shortSC;
+      setCentersList(() => scList);
+      if (serviceCenterIdFromParams) {
+        const selectedSc = scList.find(sc => sc.id === Number(serviceCenterIdFromParams));
+        if (selectedSc) {
+          selectSc(selectedSc);
+        } else {
+          console.info("We can't find service center in serviceCenterList", scList);
+        }
+      } else {
+        console.info('We do not have serviceCenterId from params (URL)');
+      }
     }
   }, [currentUser, shortSC, restrictedRoles]);
 
@@ -54,6 +91,33 @@ const ServiceCenterSelect = () => {
   useEffect(() => {
     dispatch(setTrackerCreated({ isCreated: false, ids: [] }));
   }, []);
+
+  const selectSc = (sc: IServiceCenter) => {
+    dispatch(loadSCProfile(sc.id));
+    dispatch(clearAppointmentData());
+    dispatch(setServiceOptionChanged(false));
+    dispatch(setCustomerEnteredEmail(''));
+    dispatch(setCustomerSearchData(initialCustomerSearch));
+    dispatch(setAddress(null));
+    dispatch(setZipCode(''));
+    dispatch(setSideBarSteps([]));
+    dispatch(setVehicle(null));
+    dispatch(setCustomerLoadedData(null));
+    dispatch(setServiceTypeOption(null));
+    dispatch(setWelcomeScreenView('select'));
+    const encoded = encodeSCID(sc.id);
+    const params = new URLSearchParams({
+      frame: '1',
+    });
+
+    if (serviceCenterIdFromParams) params.set(SERVICE_CENTER_ID, serviceCenterIdFromParams);
+    if (contactFromParams) params.set(CONTACT, contactFromParams);
+    if (firstNameFromParams) params.set(FIRST_NAME, firstNameFromParams);
+    if (lastNameFromParams) params.set(LAST_NAME, lastNameFromParams);
+    if (vinCodeFromParams) params.set(VIN, vinCodeFromParams);
+
+    history.push(`${Routes.EndUser.Welcome}/${encoded}?${params.toString()}`);
+  };
 
   return !scProfile || isProfileLoading || shortLoading ? (
     <Loading />
---
+++ src/types/URLQueryType.ts
@@ -0,0 +1,5 @@
+export const SERVICE_CENTER_ID = 'scId';
+export const CONTACT = 'contact';
+export const FIRST_NAME = 'firstName';
+export const LAST_NAME = 'lastName';
+export const VIN = 'vin';
