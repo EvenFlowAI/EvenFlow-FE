df1058b3c8a645a98d8c02e99b4a627c3bd40484
Vladyslav Nasadyk <vladyslav.nasadyk@devabit.com>
2026-05-06 18:15:37 +0300

    feat: EF-7819 handle `scId` param for auto-search in `ReturningCustomerForAdmin`
    - Added support for `SERVICE_CENTER_ID` query param in pre-filling search criteria.
    - Updated dependency array in `useEffect` to ensure proper query param handling.
    
    (cherry picked from commit c75635520fb97690be7cfc6bd33ff092fd680583)

--- src/features/booking/CustomerSelect/ReturningCustomerForAdmin/ReturningCustomerForAdmin.tsx
+++ src/features/booking/CustomerSelect/ReturningCustomerForAdmin/ReturningCustomerForAdmin.tsx
@@ -122,7 +122,7 @@ const ReturningCustomerForAdmin: React.FC<
     } else {
       console.info('No params for automatically search found');
     }
-  }, [contactFromParams]);
+  }, [vinCodeFromParams, firstNameFromParams, contactFromParams, lastNameFromParams]);
 
   const formIsValid = useMemo(() => {
     return (
